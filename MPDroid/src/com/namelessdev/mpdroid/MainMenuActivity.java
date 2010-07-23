a9bca0ab77cec3db91acf2291f11a5a59e15611c
Kent Gustavsson <nedo80@gmail.com>
2010-07-23 12:07:30 +0200

    Had lost an ) there

--- MPDroid/src/com/namelessdev/mpdroid/MainMenuActivity.java
+++ MPDroid/src/com/namelessdev/mpdroid/MainMenuActivity.java
@@ -514,7 +514,7 @@ public class MainMenuActivity extends Activity implements StatusChangeListener, 
 	
 	@Override
 	public boolean onTouchEvent ( MotionEvent event) {
-		if (gestureDetector.onTouchEvent(event) 
+		if (gestureDetector.onTouchEvent(event))
 			return true;
 		return false;
 	}
