0c11f9822bc4162e33d2f846f0692e4e24d4aa70
Kent Gustavsson <nedo80@gmail.com>
2010-07-26 22:41:39 +0200

    Added this one to to the BrowseActivity classes, two lists here for the music, probably should be reduced to one, one is needed tho so we can add the correct song through it when activated.
    Here to like in SongActivity the Add method needed overrideing, and
    added Add with index as parameter because it made reusing code so much
    simpler.

--- MPDroid/src/com/namelessdev/mpdroid/SearchSongActivity.java
+++ MPDroid/src/com/namelessdev/mpdroid/SearchSongActivity.java
@@ -22,13 +22,21 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class SearchSongActivity extends ListActivity implements AsyncExecListener {
-	private Collection<Music> items = null;
-	private ArrayList<Music> arrayItems = null;
-	private List<String> itemsList = null;
+public class SearchSongActivity extends BrowseActivity implements AsyncExecListener {
+	private ArrayList<Music> arrayMusic = null;
+	// We need this to store the music that is on display so that we can figure out what one was picked later on
+	private ArrayList<Music> dispMusic = null;
+	
 	private int iJobID = -1;
 	private ProgressDialog pd;
 	String searchKeywords = "";
+	
+	public SearchSongActivity()
+	{
+		super(R.string.addSong, R.string.songAdded, MPD.MPD_SEARCH_TITLE);
+		items = new ArrayList<String>();
+		dispMusic = new ArrayList<Music>();
+	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +57,6 @@ public class SearchSongActivity extends ListActivity implements AsyncExecListene
 		MPDApplication app = (MPDApplication)getApplication();
 		// Loading Albums asynchronous...
 		final String finalSearch = searchKeywords;
-		itemsList = new ArrayList<String>();
 		app.oMPDAsyncHelper.addAsyncExecListener(this);
 		iJobID = app.oMPDAsyncHelper.execAsync(new Runnable(){
 			@Override
@@ -57,55 +64,62 @@ public class SearchSongActivity extends ListActivity implements AsyncExecListene
 			{
 				try {
 					MPDApplication app = (MPDApplication)getApplication();
-					items = app.oMPDAsyncHelper.oMPD.search("any", finalSearch);
+					arrayMusic = new ArrayList<Music>(app.oMPDAsyncHelper.oMPD.search("any", finalSearch));
 				} catch (MPDServerException e) {	
 				}
 			}
 		});
+		
+
+		registerForContextMenu(getListView());
 	}
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-		Music music = arrayItems.get(position);
+		Add(position);
+    }
+    
+    @Override
+	protected void Add(String item) {
+    	Add(items.indexOf(item));
+	}
+	
+    protected void Add(int index)
+    {
+    	Music music = dispMusic.get(index);
 		try {
 			MPDApplication app = (MPDApplication)getApplication();
 
 			app.oMPDAsyncHelper.oMPD.getPlaylist().add(music);
-			MainMenuActivity.notifyUser(String.format(getResources().getString(R.string.songAdded,music.getTitle()),music.getName()), SearchSongActivity.this);
+			MainMenuActivity.notifyUser(String.format(getResources().getString(R.string.songAdded,music.getTitle()),music.getName()), this);
 		} catch (MPDServerException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     }
-	
-   
+    
 	public void asyncExecSucceeded(int jobID) {
 		// TODO Auto-generated method stub
 		if(iJobID == jobID)
 		{
 			searchKeywords = searchKeywords.toLowerCase().trim();
-			arrayItems = new ArrayList<Music>(items);
-			for (Music music : arrayItems) {
+			
+			for (Music music : arrayMusic) {
 				if(music.getTitle().toLowerCase().contains(searchKeywords))
-					itemsList.add(music.getTitle());
+				{
+					items.add(music.getTitle());
+					dispMusic.add(music);
+				}
 			}
 			
 			// Use the ListViewButtonAdapter class to show the albums
-			ListViewButtonAdapter<String> almumsAdapter = new ListViewButtonAdapter<String>(SearchSongActivity.this, android.R.layout.simple_list_item_1, itemsList);
+			ListViewButtonAdapter<String> almumsAdapter = new ListViewButtonAdapter<String>(SearchSongActivity.this, android.R.layout.simple_list_item_1, items);
 			
 			PlusListener AddListener = new PlusListener() {
 				@Override
 				public void OnAdd(CharSequence sSelected, int iPosition)
 				{
-					Music music = arrayItems.get(iPosition);
-					try {
-						MPDApplication app = (MPDApplication)getApplication();
-						app.oMPDAsyncHelper.oMPD.getPlaylist().add(music);
-						MainMenuActivity.notifyUser(String.format(getResources().getString(R.string.songAdded),sSelected), SearchSongActivity.this);
-					} catch (MPDServerException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+					Add(iPosition);
 				}
 			};
 			
