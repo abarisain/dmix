7e8ec76ab23612d49f01f75d49ff998ecb9fde1e
Kent Gustavsson <nedo80@gmail.com>
2010-07-28 13:49:52 +0200

    This view now also uses the BrowseActivitys async handling, didn't have loading screen before so added that and the relevant string in strings.xml

--- MPDroid/src/com/namelessdev/mpdroid/SongsActivity.java
+++ MPDroid/src/com/namelessdev/mpdroid/SongsActivity.java
@@ -12,6 +12,7 @@ import com.namelessdev.mpdroid.R;
 import com.namelessdev.mpdroid.R.layout;
 import com.namelessdev.mpdroid.R.string;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -31,41 +32,37 @@ public class SongsActivity extends BrowseActivity {
 		super.onCreate(icicle);
 		setContentView(R.layout.artists);
 
-		try {
-			MPDApplication app = (MPDApplication)getApplication();
-			String album = (String) this.getIntent().getStringExtra("album");
-			this.setTitle(album);
-			dispMusic = new ArrayList<Music>(app.oMPDAsyncHelper.oMPD.find(MPD.MPD_FIND_ALBUM, album));
-
-			for (Music music : dispMusic) {
-				items.add(music.getTitle());
-			}
+		MPDApplication app = (MPDApplication)getApplication();
+		String album = (String) this.getIntent().getStringExtra("album");
+		this.setTitle(album);
+		
+		pd = ProgressDialog.show(this, getResources().getString(R.string.loading), getResources().getString(R.string.loadingSongs));
 
 
-			// Put the list on the screen...
-			ListViewButtonAdapter<String> fsentries = new ListViewButtonAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-			PlusListener AddListener = new PlusListener() {
-				@Override
-				public void OnAdd(CharSequence sSelected, int iPosition)
-				{
-					Music music = dispMusic.get(iPosition);
-					try {
-						MPDApplication app = (MPDApplication)getApplication();
-						app.oMPDAsyncHelper.oMPD.getPlaylist().add(music);
-						MainMenuActivity.notifyUser(String.format(getResources().getString(R.string.songAdded),sSelected), SongsActivity.this);
-					} catch (MPDServerException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
+		// Loading Albums asynchronous...
+		app.oMPDAsyncHelper.addAsyncExecListener(this);
+		Runnable run = new Runnable(){
+			@Override
+			public void run() {
+				try {
+					MPDApplication app = (MPDApplication)getApplication();
+					dispMusic = new ArrayList<Music>(app.oMPDAsyncHelper.oMPD.find(MPD.MPD_FIND_ALBUM, album));
+				} catch (MPDServerException e) {
+					
 				}
-			};
-			fsentries.SetPlusListener(AddListener);
-			setListAdapter(fsentries);
-		} catch (MPDServerException e) {
-			e.printStackTrace();
-			this.setTitle(e.getMessage());
-		}
+			}
+			
+			String album;
+			public Runnable setAlbum(String album)
+			{
+				this.album = album;
+				return this;
+			}
+		}.setAlbum(album);
+		
+		iJobID = app.oMPDAsyncHelper.execAsync(run);
+		
+		
 		registerForContextMenu(getListView());	
 	}
     
@@ -74,14 +71,11 @@ public class SongsActivity extends BrowseActivity {
     	Add(items.indexOf(item));
 	}
     
-	@Override
-	/**
-	 * We also listen to item clicks here...
-	 */
+    @Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		Add(position);
 	}    
-
+    
     protected void Add(int index) {
     	Music music = dispMusic.get(index);
 		try {
@@ -95,4 +89,15 @@ public class SongsActivity extends BrowseActivity {
 		}
     }
 
+	@Override
+	public void asyncExecSucceeded(int jobID) {
+		if(iJobID == jobID) {
+			for (Music music : dispMusic) {
+				items.add(music.getTitle());
+			}
+			
+			super.asyncExecSucceeded(jobID);
+		}
+	}
+    
 }
