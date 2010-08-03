3e3909c9a04dc5aed43ba51fdb855c054782ae9f
Kent Gustavsson <nedo80@gmail.com>
2010-08-04 01:02:28 +0200

    Had forgotten to add these files...

---
+++ MPDroid/src/com/namelessdev/mpdroid/PlaylistSaveActivity.java
@@ -0,0 +1,54 @@
+package com.namelessdev.mpdroid;
+
+import org.a0z.mpd.MPDServerException;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class PlaylistSaveActivity extends Activity implements OnClickListener{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.playlist_save);
+		((Button)findViewById(R.id.save)).setOnClickListener(this);
+
+	}
+	
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.save:
+				try {
+					MPDApplication app = (MPDApplication)getApplication();
+					app.oMPDAsyncHelper.oMPD.getPlaylist().save(((EditText) findViewById(R.id.editPlaylistName)).getText().toString());
+				} catch (MPDServerException e) {
+				}
+			
+				finish();
+				break;
+		}
+	}
+	
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		MPDApplication app = (MPDApplication)getApplicationContext();
+		app.setActivity(this);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		MPDApplication app = (MPDApplication)getApplicationContext();
+		app.unsetActivity(this);
+	}
+	
+
+}
