2ac4cd9710ffb2061ab97a2516d814698bc70df6
Arnaud Barisain Monrose <abarisain@gmail.com>
2012-07-07 11:05:22 +0200

    Comparaison now ignores case

--- JMPDComm/src/org/a0z/mpd/Item.java
+++ JMPDComm/src/org/a0z/mpd/Item.java
@@ -14,7 +14,7 @@ public abstract class Item implements Comparable<Item> {
 
 	@Override
     public int compareTo(Item o) {
-		return getName().compareTo(o.getName());
+		return getName().compareToIgnoreCase(o.getName());
 	}
 
 	@Override
