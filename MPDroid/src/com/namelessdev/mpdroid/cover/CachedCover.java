a103002307a611ea40d8f036034d6cfcf3fce5f9
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-02-11 14:37:17 -0500

    Added a non working local cover art provider

---
+++ MPDroid/src/com/namelessdev/mpdroid/cover/CachedCover.java
@@ -0,0 +1,16 @@
+package com.namelessdev.mpdroid.cover;
+
+public class CachedCover implements ICoverRetriever {
+
+	@Override
+	public String[] getCoverUrl(String artist, String album, String path) throws Exception {
+		// TODO : Implement a configurable local cover art provider
+		return null;
+	}
+
+	@Override
+	public boolean isCoverLocal() {
+		return true;
+	}
+
+}
