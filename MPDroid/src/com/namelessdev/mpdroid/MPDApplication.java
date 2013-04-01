8ae4ed4cbaaf570983b65dec3d9eb7e40c865a69
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-03-31 23:06:56 -0400

    2.X devices won't get light theme.

--- MPDroid/src/com/namelessdev/mpdroid/MPDApplication.java
+++ MPDroid/src/com/namelessdev/mpdroid/MPDApplication.java
@@ -18,6 +18,7 @@ import android.content.DialogInterface.OnClickListener;
 import android.content.DialogInterface.OnKeyListener;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.StrictMode;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -270,6 +271,8 @@ public class MPDApplication extends Application implements ConnectionListener {
 	}
 
 	public static boolean isLightThemeSelected() {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+			return false;
 		return true;
 	}
 }
