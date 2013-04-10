2027f54b2553977a0088c544b65a31bfcb0e2359
mafro <github@mafro.net>
2013-04-11 08:02:58 +1000

    look for local artwork before Last.fm

--- MPDroid/src/com/namelessdev/mpdroid/helpers/CoverAsyncHelper.java
+++ MPDroid/src/com/namelessdev/mpdroid/helpers/CoverAsyncHelper.java
@@ -107,16 +107,16 @@ public class CoverAsyncHelper extends Handler {
 		final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(app);
 		final List<CoverRetrievers> enabledRetrievers = new ArrayList<CoverRetrievers>();
 		// There is a cover provider order, respect it.
-		// Cache -> LastFM -> MPD Server
+		// Cache -> MPD Server -> LastFM
 		if (settings.getBoolean(PREFERENCE_CACHE, true)) {
 			enabledRetrievers.add(CoverRetrievers.CACHE);
 		}
 		if (!(settings.getBoolean(PREFERENCE_ONLY_WIFI, false)) | (isWifi())) {
-			if (settings.getBoolean(PREFERENCE_LASTFM, true)) {
-				enabledRetrievers.add(CoverRetrievers.LASTFM);
-			}
 			if (settings.getBoolean(PREFERENCE_LOCALSERVER, false)) {
 				enabledRetrievers.add(CoverRetrievers.LOCAL);
+			}
+			if (settings.getBoolean(PREFERENCE_LASTFM, true)) {
+				enabledRetrievers.add(CoverRetrievers.LASTFM);
 			}
 		}
 		setCoverRetrievers(enabledRetrievers);
