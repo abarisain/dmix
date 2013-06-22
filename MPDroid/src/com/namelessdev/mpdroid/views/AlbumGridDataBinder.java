a9f134da1e17e3accafa02df70ea3ea493103428
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-06-22 15:01:30 -0400

    Removed useless toast

--- MPDroid/src/com/namelessdev/mpdroid/views/AlbumGridDataBinder.java
+++ MPDroid/src/com/namelessdev/mpdroid/views/AlbumGridDataBinder.java
@@ -2,27 +2,18 @@ package com.namelessdev.mpdroid.views;
 
 import java.util.List;
 
-import org.a0z.mpd.Artist;
 import org.a0z.mpd.Album;
 import org.a0z.mpd.Item;
-import org.a0z.mpd.Music;
-import org.a0z.mpd.exception.MPDServerException;
 
-import android.util.DisplayMetrics;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.preference.PreferenceManager;
 import android.view.View;
 
 import com.namelessdev.mpdroid.MPDApplication;
 import com.namelessdev.mpdroid.R;
-import com.namelessdev.mpdroid.cover.CachedCover;
-import com.namelessdev.mpdroid.helpers.CoverAsyncHelper;
 import com.namelessdev.mpdroid.helpers.AlbumCoverDownloadListener;
-import com.namelessdev.mpdroid.tools.Tools;
+import com.namelessdev.mpdroid.helpers.CoverAsyncHelper;
 import com.namelessdev.mpdroid.views.holders.AbstractViewHolder;
 import com.namelessdev.mpdroid.views.holders.AlbumViewHolder;
 
@@ -45,11 +36,6 @@ public class AlbumGridDataBinder extends AlbumDataBinder {
 
 		// display the album title
 		holder.albumName.setText(album.getName());
-
-		// display message if wifi off
-		if(coverHelper.isWifi() == false) {
-			Tools.notifyUser(context.getResources().getString(R.string.albumGridWifiUnavailable), context);
-		}
 
 		// listen for new artwork to be loaded
 		coverHelper.addCoverDownloadListener(new AlbumCoverDownloadListener(context, holder.albumCover));
