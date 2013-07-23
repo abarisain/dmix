da4c4fccd4e5a9eaa3ce45166f1cac4e097a1b38
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-07-22 21:01:55 -0400

    Removed useless code. I think.

--- MPDroid/src/com/namelessdev/mpdroid/views/BaseDataBinder.java
+++ MPDroid/src/com/namelessdev/mpdroid/views/BaseDataBinder.java
@@ -11,7 +11,6 @@ import android.view.View;
 
 import com.namelessdev.mpdroid.MPDApplication;
 import com.namelessdev.mpdroid.adapters.ArrayIndexerDataBinder;
-import com.namelessdev.mpdroid.cover.CachedCover;
 import com.namelessdev.mpdroid.helpers.CoverAsyncHelper;
 import com.namelessdev.mpdroid.views.holders.AbstractViewHolder;
 
@@ -53,31 +52,7 @@ public abstract class BaseDataBinder implements ArrayIndexerDataBinder {
 	}
 
 	protected void loadArtwork(CoverAsyncHelper coverHelper, String artist, String album) {
-		// Don't forget to mirror your changes in StoredPlaylistDataBinder, haven't found a nice way to factor this
-		boolean haveCachedArtwork = false;
-
-		if (enableCache) {
-			try {
-				CachedCover cachedCover = new CachedCover(app);
-				final String[] urls = cachedCover.getCoverUrl(artist, album, null, null);
-				if ((urls != null) && (urls.length > 0)) {
-					haveCachedArtwork = true;
-				}
-			} catch (Exception e) {
-				// no cached artwork available
-			}
-		}
-
-		// Did we find a cached cover ? If yes, skip the download
-		// Only continue if we are not on WiFi and Cellular download is enabled
-		if (!haveCachedArtwork) {
-			// If we are not on WiFi and Cellular download is enabled
-			if (coverHelper.isWifi() || !onlyDownloadOnWifi) {
-				coverHelper.downloadCover(null, album, null, null);
-			}
-		} else {
-			coverHelper.downloadCover(artist, album, null, null);
-		}
+		coverHelper.downloadCover(artist, album, null, null);
 	}
 
 }
