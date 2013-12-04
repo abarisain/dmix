abarisain/dmix

commit bf317081cf3ab2050e915c07934c8d776a420794

remove duplicate artist specification for album requests
Album already contains Artist info
abarisain/dmix/#322
This does not solve the missing cover in album list

JMPDComm/src/org/a0z/mpd/MPD.java
@@ -1156,16 +1156,54 @@ public class MPD {
         mpdConnection.sendCommand(MPDCommand.MPD_CMD_OUTPUTDISABLE, Integer.toString(id));
     }
 
-    public List<Music> getSongs(Artist artist, Album album) throws MPDServerException {
-        List<Music> aasongs = getSongs(artist, album, true); // album artist
+    public List<Music> getSongs(Album album) throws MPDServerException {
+        List<Music> aasongs = getSongs(album, true); // album artist
         if (aasongs == null || aasongs.size() == 0)
-            return  getSongs(artist, album, false); // artist
+            return  getSongs(album, false); // artist
         else
             return aasongs;
     }
 
-    public List<Music> getSongs(Artist artist, Album album, boolean useAlbumArtist) throws MPDServerException {
-        boolean haveArtist = (null != artist);
+    public List<Music> getSongs(Artist artist) throws MPDServerException {
+        String[] search = new String[2];
+        // try as album artist
+        search[0] = MPDCommand.MPD_TAG_ALBUM_ARTIST;
+        search[1] = artist.getName();
+        List<Music> songs = find(search);
+        if (null == songs) {
+            // none found -> use artist search
+            search[0] = MPDCommand.MPD_FIND_ARTIST;
+            songs = find(search);
+        }
+        if (null != songs) {
+            Collections.sort(songs);
+        }
+        return songs;
+    }
+
+    public List<Music> getSongs(Album album, boolean useAlbumArtist) throws MPDServerException {
+        Artist artist = album.getArtist();
+        boolean haveArtist = (null != album.getArtist());
+        String[] search = new String[2];
+        search[0] = MPDCommand.MPD_FIND_ALBUM;
+        search[1] = album.getName();
+        List<Music> songs=find(search);
+        if(album instanceof UnknownAlbum) {
+            // filter out any songs with which have the album tag set
+            Iterator<Music> iter = songs.iterator();
+            while (iter.hasNext()) {
+                if (iter.next().getAlbum() != null) iter.remove();
+            }
+        }
+        if (null!=songs) {
+            Collections.sort(songs);
+        }
+        return songs;
+    }
+
+    public List<Music> getSongsOld(Artist Artist, Album album, boolean useAlbumArtist) throws MPDServerException {
+        Artist artist = album.getArtist();
+        boolean haveArtist = (null != album.getArtist());
         boolean haveAlbum = (null != album) && !(album instanceof UnknownAlbum);
         String[] search = null;
 
@@ -1360,26 +1398,14 @@ public class MPD {
         getMpdConnection().sendCommand(MPDCommand.MPD_CMD_PLAYLIST_DEL, playlistName, Integer.toString(pos));
     }
 
-    public void addToPlaylist(String playlistName, Collection<Music> c) throws MPDServerException {
-        if (null==c || c.size()<1) {
-            return;
-        }
-        for (Music m : c) {
-            getMpdConnection().queueCommand(MPDCommand.MPD_CMD_PLAYLIST_ADD, playlistName, m.getFullpath());
-        }
-        getMpdConnection().sendCommandQueue();
-    }
 
-    public void addToPlaylist(String playlistName, FilesystemTreeEntry entry) throws MPDServerException {
-        getMpdConnection().sendCommand(MPDCommand.MPD_CMD_PLAYLIST_ADD, playlistName, entry.getFullpath());
-    }
 
     public void add(Artist artist) throws MPDServerException {
         add(artist, false, false);
     }
 
-    public void add(Artist artist, Album album) throws MPDServerException {
-        add(artist, album, false, false);
+    public void add(Album album) throws MPDServerException {
+        add(album, false, false);
     }
 
     public void add(Music music) throws MPDServerException {
@@ -1390,8 +1416,9 @@ public class MPD {
         add(playlist, false, false);
     }
 
-    public void add(Artist artist, boolean replace, boolean play) throws MPDServerException {
-        add(artist, null, replace, play);
+    public void add(Artist artist,
+                    boolean replace, boolean play) throws MPDServerException {
+        add(artist, replace, play);
     }
 
     public void add(final FilesystemTreeEntry music, boolean replace, boolean play) throws MPDServerException {
@@ -1413,12 +1440,12 @@ public class MPD {
         add(r, replace, play);
     }
 
-    public void add(final Artist artist, final Album album, boolean replace, boolean play) throws MPDServerException {
+    public void add(final Album album, boolean replace, boolean play) throws MPDServerException {
         final Runnable r = new Runnable() {
                 @Override
                     public void run() {
                     try {
-                        final ArrayList<Music> songs = new ArrayList<Music>(getSongs(artist, album));
+                        final ArrayList<Music> songs = new ArrayList<Music>(getSongs(album));
                         getPlaylist().addAll(songs);
                     } catch (MPDServerException e) {
                         e.printStackTrace();
@@ -1511,22 +1538,37 @@ public class MPD {
         }
     }
 
+    public void addToPlaylist(String playlistName, Collection<Music> c) throws MPDServerException {
+        if (null==c || c.size()<1) {
+            return;
+        }
+        for (Music m : c) {
+            getMpdConnection().queueCommand(MPDCommand.MPD_CMD_PLAYLIST_ADD, playlistName, m.getFullpath());
+        }
+        getMpdConnection().sendCommandQueue();
+    }
+
+    public void addToPlaylist(String playlistName, FilesystemTreeEntry entry) throws MPDServerException {
+        getMpdConnection().sendCommand(MPDCommand.MPD_CMD_PLAYLIST_ADD, playlistName, entry.getFullpath());
+    }
+
     public void addToPlaylist(String playlistName, Artist artist) throws MPDServerException {
-        addToPlaylist(playlistName, artist, null);
+        addToPlaylist(playlistName, new ArrayList<Music>(getSongs(artist)));
     }
 
     public void addToPlaylist(String playlistName, Album album) throws MPDServerException {
-        addToPlaylist(playlistName, null, album);
+        addToPlaylist(playlistName, new ArrayList<Music>(getSongs(album)));
     }
 
-    public void addToPlaylist(String playlistName, Artist artist, Album album) throws MPDServerException {
-        addToPlaylist(playlistName, new ArrayList<Music>(getSongs(artist, album)));
-    }
+    // public void addToPlaylist(String playlistName, Artist artist, Album album) throws MPDServerException {
+    // 	addToPlaylist(playlistName, new ArrayList<Music>(getSongs(artist, album)));
+    // }
 
     public void addToPlaylist(String playlistName, Music music) throws MPDServerException {
         final ArrayList<Music> songs = new ArrayList<Music>();
         songs.add(music);
         addToPlaylist(playlistName, songs);
     }
+
 
 }

MPDroid/src/com/namelessdev/mpdroid/SearchActivity.java
@@ -234,18 +234,23 @@ public class SearchActivity extends MPDroidActivity implements OnMenuItemClickLi
         if (object instanceof Music) {
             add((Music) object, replace, play);
         } else if (object instanceof Artist) {
-            add(((Artist) object), null, replace, play);
+            add(((Artist) object), replace, play);
         } else if (object instanceof Album) {
             Album album = (Album) object;
-            add(album.getArtist(), album, replace, play);
+            add(album, replace, play);
         }
     }
 
-    protected void add(Artist artist, Album album, boolean replace, boolean play) {
+    protected void add(Album album, boolean replace, boolean play) {
         try {
-            app.oMPDAsyncHelper.oMPD.add(artist, album, replace, play);
-            Tools.notifyUser(String.format(getResources().getString(addedString), null == album ? artist.getName()
-                    : (null == artist ? album.getName() : artist.getName() + " - " + album.getName())), this);
+            app.oMPDAsyncHelper.oMPD.add(album, replace, play);
+            String notifystr = album.getArtist().getName();
+            if (notifystr == null) {
+                notifystr = album.getName();
+            } else {
+                notifystr += " - " + album.getName();
+            }
+            Tools.notifyUser(String.format(getResources().getString(addedString), notifystr), this);
         } catch (MPDServerException e) {
             e.printStackTrace();
         }

MPDroid/src/com/namelessdev/mpdroid/fragments/AlbumsFragment.java
@@ -63,7 +63,7 @@ public class AlbumsFragment extends BrowseFragment {
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View v, int position, long id) {
-        ((ILibraryFragmentActivity) getActivity()).pushLibraryFragment(new SongsFragment().init(artist, (Album) items.get(position)),
+        ((ILibraryFragmentActivity) getActivity()).pushLibraryFragment(new SongsFragment().init((Album) items.get(position)),
                 "songs");
     }
 
@@ -87,7 +87,7 @@ public class AlbumsFragment extends BrowseFragment {
     @Override
     protected void add(Item item, boolean replace, boolean play) {
         try {
-            app.oMPDAsyncHelper.oMPD.add(artist, (Album) item, replace, play);
+            app.oMPDAsyncHelper.oMPD.add((Album) item, replace, play);
             Tools.notifyUser(String.format(getResources().getString(irAdded), item), getActivity());
         } catch (MPDServerException e) {
             e.printStackTrace();
@@ -97,7 +97,7 @@ public class AlbumsFragment extends BrowseFragment {
     @Override
     protected void add(Item item, String playlist) {
         try {
-            app.oMPDAsyncHelper.oMPD.addToPlaylist(playlist, artist, ((Album) item));
+            app.oMPDAsyncHelper.oMPD.addToPlaylist(playlist, ((Album) item));
             Tools.notifyUser(String.format(getResources().getString(irAdded), item), getActivity());
         } catch (MPDServerException e) {
             e.printStackTrace();

MPDroid/src/com/namelessdev/mpdroid/fragments/SongsFragment.java
@@ -27,7 +27,7 @@ public class SongsFragment extends BrowseFragment {
     private static final int POPUP_COVER_SELECTIVE_CLEAN = 6;
 
     Album album = null;
-    Artist artist = null;
+    //Artist artist = null;
     TextView headerArtist;
     TextView headerInfo;
 
@@ -48,11 +48,10 @@ public class SongsFragment extends BrowseFragment {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         if (icicle != null)
-            init((Artist) icicle.getParcelable(EXTRA_ARTIST), (Album) icicle.getParcelable(EXTRA_ALBUM));
+            init((Album) icicle.getParcelable(EXTRA_ALBUM));
     }
 
-    public SongsFragment init(Artist ar, Album al) {
-        artist = ar;
+    public SongsFragment init(Album al) {
         album = al;
         return this;
     }
@@ -157,7 +156,7 @@ public class SongsFragment extends BrowseFragment {
                                 break;
                         }
                         try {
-                            app.oMPDAsyncHelper.oMPD.add(artist, album, replace, play);
+                            app.oMPDAsyncHelper.oMPD.add(album, replace, play);
                             Tools.notifyUser(String.format(getResources().getString(R.string.albumAdded), album), getActivity());
                         } catch (MPDServerException e) {
                             e.printStackTrace();
@@ -212,7 +211,7 @@ public class SongsFragment extends BrowseFragment {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         outState.putParcelable(EXTRA_ALBUM, album);
-        outState.putParcelable(EXTRA_ARTIST, artist);
+        //outState.putParcelable(EXTRA_ARTIST, artist);
         super.onSaveInstanceState(outState);
     }
 
@@ -254,7 +253,7 @@ public class SongsFragment extends BrowseFragment {
         try {
             if (getActivity() == null)
                 return;
-            items = app.oMPDAsyncHelper.oMPD.getSongs(artist, album);
+            items = app.oMPDAsyncHelper.oMPD.getSongs(album);
         } catch (MPDServerException e) {
             e.printStackTrace();
         }
@@ -272,7 +271,8 @@ public class SongsFragment extends BrowseFragment {
                 coverArt.setTag(album.getAlbumInfo().getKey());
                 coverHelper.downloadCover(album.getAlbumInfo());
             } else {
-                coverArtListener.onCoverNotFound(new CoverInfo(artistName, album.getName()));
+                //                coverArtListener.onCoverNotFound(new CoverInfo(artistName, album.getName()));
+                coverArtListener.onCoverNotFound(new CoverInfo(album.getAlbumInfo()));
             }
         }
 

MPDroid/src/com/namelessdev/mpdroid/library/SimpleLibraryActivity.java
@@ -50,7 +50,7 @@ public class SimpleLibraryActivity extends MPDroidFragmentActivity implements IL
 					}
 					rootFragment = af;
 				} else if (targetElement instanceof Album) {
-					rootFragment = new SongsFragment().init((Artist) getIntent().getParcelableExtra(EXTRA_ARTIST), (Album) targetElement);
+					rootFragment = new SongsFragment().init((Album) targetElement);
 				} else if (targetElement instanceof String) {
 					rootFragment = new FSFragment().init((String) targetElement);
 				}

commit 3c6aa570cf1ed12349a1db8122bb3aa5daeab297

make MPD search for album with artist again
to discriminate multiple albums with same name but different artists
it was broken in bf317081cf3ab2050e915c07934c8d776a420794

JMPDComm/src/org/a0z/mpd/MPD.java
@@ -1183,10 +1183,14 @@ public class MPD {
 
     public List<Music> getSongs(Album album, boolean useAlbumArtist) throws MPDServerException {
         Artist artist = album.getArtist();
-        boolean haveArtist = (null != album.getArtist());
-        String[] search = new String[2];
+        boolean haveArtist = (null != album.getArtist()) && !(album.getArtist() instanceof UnknownArtist);
+        String[] search = new String[haveArtist ? 4 : 2];
         search[0] = MPDCommand.MPD_FIND_ALBUM;
         search[1] = album.getName();
+        if (haveArtist) {
+            search[2] = useAlbumArtist ? MPDCommand.MPD_TAG_ALBUM_ARTIST : MPDCommand.MPD_FIND_ARTIST;
+            search[3] = artist.getName();
+        }
         List<Music> songs=find(search);
         if(album instanceof UnknownAlbum) {
             // filter out any songs with which have the album tag set