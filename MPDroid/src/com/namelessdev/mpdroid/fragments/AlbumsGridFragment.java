b66fb1cb91b44e520b512294335d48e2df888d46
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-12-08 22:19:58 +0100

    Album list has a smart fastscroll

--- MPDroid/src/com/namelessdev/mpdroid/fragments/AlbumsGridFragment.java
+++ MPDroid/src/com/namelessdev/mpdroid/fragments/AlbumsGridFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.GridView;
 import android.widget.ListAdapter;
 import android.widget.ProgressBar;
@@ -16,6 +17,10 @@ import com.namelessdev.mpdroid.adapters.ArrayAdapter;
 import com.namelessdev.mpdroid.views.AlbumGridDataBinder;
 
 public class AlbumsGridFragment extends AlbumsFragment {
+
+    // Minimum number of songs in the queue before the fastscroll thumb is shown
+    private static final int MIN_ITEMS_BEFORE_FASTSCROLL = 6;
+
     public AlbumsGridFragment(Artist artist) {
         super(artist);
         isCountPossiblyDisplayed = false;
@@ -41,6 +46,12 @@ public class AlbumsGridFragment extends AlbumsFragment {
     }
 
     @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        refreshFastScrollStyle();
+    }
+
+    @Override
     protected ListAdapter getCustomListAdapter() {
         if (items != null) {
             return new ArrayAdapter(getActivity(),
@@ -49,4 +60,22 @@ public class AlbumsGridFragment extends AlbumsFragment {
         return super.getCustomListAdapter();
     }
 
+    @Override
+    public void updateFromItems() {
+        super.updateFromItems();
+        refreshFastScrollStyle();
+    }
+
+    private void refreshFastScrollStyle() {
+        if (items != null && items.size() >= MIN_ITEMS_BEFORE_FASTSCROLL) {
+            // No need to enable FastScroll, this setter enables it.
+            list.setFastScrollAlwaysVisible(true);
+            list.setScrollBarStyle(AbsListView.SCROLLBARS_INSIDE_INSET);
+        } else {
+            list.setFastScrollAlwaysVisible(false);
+            list.setFastScrollEnabled(false);
+            // Matches the XML style
+            list.setScrollBarStyle(AbsListView.SCROLLBARS_OUTSIDE_OVERLAY);
+        }
+    }
 }
