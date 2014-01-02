7bc7cc30689de4733f333ed615cd8f0f3045f1fe
Avuton Olrich <avuton@gmail.com>
2014-01-02 09:44:32 -0800

    ViewGroup.LayoutParams.FILL_PARENT is depreciated.
    ViewGroup.LayoutParams.FILL_PARENT has been depreciated for drop in replacement
    ViewGroup.LayoutParams.MATCH_PARENT.

--- DragSortListView/src/com/mobeta/android/dslv/DragSortItemView.java
+++ DragSortListView/src/com/mobeta/android/dslv/DragSortItemView.java
@@ -30,7 +30,7 @@ public class DragSortItemView extends ViewGroup {
 
         // always init with standard ListView layout params
         setLayoutParams(new AbsListView.LayoutParams(
-                ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT));
 
         //setClipChildren(true);
