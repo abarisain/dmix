5c9c6cc8795292c5a2c6f588cdd01ae37605f1c8
Benoit Duffez <benoit@bicou.net>
2014-01-06 23:43:46 +0100

    fixed a lot of stuff regarding the notification and the remote control client

--- MPDroid/src/com/namelessdev/mpdroid/MusicService.java
+++ MPDroid/src/com/namelessdev/mpdroid/MusicService.java
@@ -20,16 +20,16 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaMetadataRetriever;
 import android.media.RemoteControlClient;
 import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.namelessdev.mpdroid.models.MusicParcelable;
@@ -66,6 +66,8 @@ public class MusicService extends Service implements MusicFocusable {
      */
     public static final String EXTRA_CURRENT_MUSIC = "com.namelessdev.mpdroid.MusicService.CurrentMusic";
 
+    Music mCurrentMusic = null, mPreviousMusic = null;
+
     // our AudioFocusHelper object, if it's available (it's available on SDK level >= 8)
     // If not available, this will be null. Always check for null before using!
     AudioFocusHelper mAudioFocusHelper = null;
@@ -89,32 +91,26 @@ public class MusicService extends Service implements MusicFocusable {
     // SDK level >= 14, if they're available.
     RemoteControlClient mRemoteControlClient;
 
-    // Dummy album art we will pass to the remote control (if the APIs are available).
-    Bitmap mDummyAlbumArt;
-
     // The component name of MusicIntentReceiver, for use with media button and remote control APIs
     ComponentName mMediaButtonReceiverComponent;
 
     AudioManager mAudioManager;
     NotificationManager mNotificationManager;
-
     Notification mNotification = null;
 
     @Override
     public void onCreate() {
-        Log.i(TAG, "debug: Creating service");
+        Log.d(TAG, "Creating service");
 
         mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
 
-        // create the Audio Focus Helper, if the Audio Focus feature is available (SDK 8 or above)
-        if (android.os.Build.VERSION.SDK_INT >= 8)
-            mAudioFocusHelper = new AudioFocusHelper(getApplicationContext(), this);
-        else mAudioFocus = AudioFocus.Focused; // no focus feature, so we always "have" audio focus
+        // Create the Audio Focus Helper
+        mAudioFocusHelper = new AudioFocusHelper(getApplicationContext(), this);
 
-        mDummyAlbumArt = BitmapFactory.decodeResource(getResources(), R.drawable.no_cover_art_light);
-
+        // Use the media button APIs (if available) to register ourselves for media button events
         mMediaButtonReceiverComponent = new ComponentName(this, RemoteControlReceiver.class);
+        mAudioManager.registerMediaButtonEventReceiver(mMediaButtonReceiverComponent);
     }
 
     /**
@@ -125,6 +121,8 @@ public class MusicService extends Service implements MusicFocusable {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String action = intent.getAction();
+
+        Log.d(TAG, "received command, action=" + action + " from intent: " + intent);
 
         if (action == null) {
             return START_NOT_STICKY;
@@ -190,10 +188,6 @@ public class MusicService extends Service implements MusicFocusable {
             }
         }.execute((MPDApplication) getApplication());
 
-        // Tell any remote controls that our playback state is 'playing'.
-        if (mRemoteControlClient != null) {
-            mRemoteControlClient.setPlaybackState(RemoteControlClient.PLAYSTATE_PLAYING);
-        }
         updatePlayingInfo(RemoteControlClient.PLAYSTATE_PLAYING);
     }
 
@@ -213,16 +207,16 @@ public class MusicService extends Service implements MusicFocusable {
         }).start();
         relaxResources(); // while paused, we always retain the MediaPlayer do not give up audio focus
 
-        // Tell any remote controls that our playback state is 'paused'.
-        if (mRemoteControlClient != null) {
-            mRemoteControlClient.setPlaybackState(RemoteControlClient.PLAYSTATE_PAUSED);
-        }
         updatePlayingInfo(RemoteControlClient.PLAYSTATE_PAUSED);
     }
 
     void processUpdateInfo(MusicParcelable music) {
-        Log.d(TAG, "parcelable=" + music);
-        updatePlayingInfo(StreamingService.PLAYSTATE_PLAYING, music);
+        Log.d(TAG, "parcelable=" + music + " mCurrentMusic=" + mCurrentMusic);
+        if (mCurrentMusic != null && mCurrentMusic.equals((Music) music)) {
+            return;
+        }
+        mCurrentMusic = music;
+        updatePlayingInfo(StreamingService.PLAYSTATE_PLAYING);
     }
 
     void processRewindRequest() {
@@ -308,94 +302,124 @@ public class MusicService extends Service implements MusicFocusable {
     }
 
     void updatePlayingInfo(int state) {
-        updatePlayingInfo(state, null);
-    }
+        Log.d(TAG, "update playing info: state=" + state + " (previous state: " + mPreviousState + "), music=" + mCurrentMusic + " (previous music: " + mPreviousMusic + ")");
+        boolean updateNotification = false;
 
-    void updatePlayingInfo(int state, Music currentMusic) {
-        relaxResources(); // release everything except MediaPlayer
-
-        try {
-            // Use the media button APIs (if available) to register ourselves for media button events
-            MediaButtonHelper.registerMediaButtonEventReceiverCompat(mAudioManager, mMediaButtonReceiverComponent);
-
-            // Use the remote control APIs (if available) to set the playback state
-            if (mRemoteControlClient == null) {
-                Intent intent = new Intent(Intent.ACTION_MEDIA_BUTTON);
-                intent.setComponent(mMediaButtonReceiverComponent);
-                mRemoteControlClient = new RemoteControlClient(PendingIntent.getBroadcast(this /*context*/, 0 /*requestCode, ignored*/, intent /*intent*/, 0 /*flags*/));
-                mAudioManager.registerRemoteControlClient(mRemoteControlClient);
-            }
-
-            mRemoteControlClient.setPlaybackState(state);
+        // Create the remote control client
+        if (mRemoteControlClient == null) {
+            Intent intent = new Intent(Intent.ACTION_MEDIA_BUTTON);
+            intent.setComponent(mMediaButtonReceiverComponent);
+            mRemoteControlClient = new RemoteControlClient(PendingIntent.getBroadcast(this /*context*/, 0 /*requestCode, ignored*/, intent /*intent*/, 0 /*flags*/));
             mRemoteControlClient.setTransportControlFlags(RemoteControlClient.FLAG_KEY_MEDIA_PLAY |
                     RemoteControlClient.FLAG_KEY_MEDIA_PAUSE |
                     RemoteControlClient.FLAG_KEY_MEDIA_PREVIOUS |
                     RemoteControlClient.FLAG_KEY_MEDIA_NEXT |
                     RemoteControlClient.FLAG_KEY_MEDIA_STOP);
 
-            Music music = currentMusic;
-            if (currentMusic == null) {
-                //TODO: load this from a background thread
-                final MPDApplication app = (MPDApplication) getApplication();
-                if (app != null) {
-                    if (app.getApplicationState().streamingMode) {
-                        Intent i = new Intent(app, StreamingService.class);
-                        i.setAction("com.namelessdev.mpdroid.RESET_STREAMING");
-                        startService(i);
-                    }
+            mAudioManager.registerRemoteControlClient(mRemoteControlClient);
+        }
 
+        if (state != mPreviousState || mCurrentMusic == null) {
+            updateNotification = true;
+        }
+
+        //TODO: load this from a background thread
+        if (mCurrentMusic == null) {
+            final MPDApplication app = (MPDApplication) getApplication();
+            if (app != null) {
+                try {
                     final MPDStatus status = app.oMPDAsyncHelper.oMPD.getStatus();
                     final int songPos = status.getSongPos();
                     if (songPos >= 0) {
-                        music = app.oMPDAsyncHelper.oMPD.getPlaylist().getByIndex(songPos);
+                        mCurrentMusic = app.oMPDAsyncHelper.oMPD.getPlaylist().getByIndex(songPos);
                     }
+                } catch (MPDServerException e) {
+                    Log.w("MusicService", "MPDServerException playing next song: " + e.getMessage());
                 }
             }
-
-            if (music != null) {
-                // Update the remote controls
-                mRemoteControlClient.editMetadata(true) //
-                        .putString(MediaMetadataRetriever.METADATA_KEY_ARTIST, music.getArtist()) //
-                        .putString(MediaMetadataRetriever.METADATA_KEY_ALBUM, music.getAlbum()) //
-                        .putString(MediaMetadataRetriever.METADATA_KEY_TITLE, music.getTitle()) //
-                        .putLong(MediaMetadataRetriever.METADATA_KEY_DURATION, music.getTime()) //
-                                // TODO: fetch real item artwork
-                                //.putBitmap(RemoteControlClient.MetadataEditorCompat.METADATA_KEY_ARTWORK, mDummyAlbumArt) //
-                        .apply();
-
-                if (mNotification == null || state != mPreviousState) {
-                    setUpAsForeground(music.getArtist(), music.getTitle());
-                    updateNotification(music.getArtist(), music.getTitle());
-                }
-            }
-        } catch (MPDServerException e) {
-            Log.w("MusicService", "MPDServerException playing next song: " + e.getMessage());
         }
 
+        if (mCurrentMusic != null) {
+            // Update the remote controls
+            mRemoteControlClient.editMetadata(true) //
+                    .putString(MediaMetadataRetriever.METADATA_KEY_ARTIST, mCurrentMusic.getArtist()) //
+                    .putString(MediaMetadataRetriever.METADATA_KEY_ALBUM, mCurrentMusic.getAlbum()) //
+                    .putString(MediaMetadataRetriever.METADATA_KEY_TITLE, mCurrentMusic.getTitle()) //
+                    .putLong(MediaMetadataRetriever.METADATA_KEY_DURATION, mCurrentMusic.getTime()) //
+                            // TODO: fetch real item artwork
+                            //.putBitmap(RemoteControlClient.MetadataEditorCompat.METADATA_KEY_ARTWORK, mDummyAlbumArt) //
+                    .apply();
+        }
+
+        if (mPreviousMusic != null && mCurrentMusic != null && !mPreviousMusic.equals(mCurrentMusic)) {
+            updateNotification = true;
+        }
+
+        if (state == RemoteControlClient.PLAYSTATE_STOPPED) {
+            if (mNotificationManager != null) {
+                mNotificationManager.cancel(NOTIFICATION_ID);
+            }
+            relaxResources();
+            giveUpAudioFocus();
+            stopSelf();
+        } else /*if (updateNotification) */ {
+            // Update notification & lockscreen widget
+            updateNotification(state);
+            mRemoteControlClient.setPlaybackState(state);
+        }
+
+        mPreviousMusic = mCurrentMusic;
         mPreviousState = state;
     }
 
     /**
      * Updates the notification.
      */
-    void updateNotification(String artist, String title) {
-        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(getApplicationContext(), MainMenuActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
-        mNotification.setLatestEventInfo(getApplicationContext(), artist, title, pi);
-        mNotificationManager.notify(NOTIFICATION_ID, mNotification);
-    }
+    void updateNotification(int state) {
+        Log.d(TAG, "update notification: " + mCurrentMusic.getArtist() + " - " + mCurrentMusic.getTitle() + ", state: " + state);
 
-    /**
-     * Configures service as a foreground service. A foreground service is a service that's doing
-     * something the user is actively aware of (such as playing music), and must appear to the
-     * user as a notification. That's why we create the notification here.
-     */
-    void setUpAsForeground(String artist, String title) {
-        PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(getApplicationContext(), MainMenuActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
-        mNotification = new Notification();
-        mNotification.tickerText = title;
-        mNotification.icon = R.drawable.icon_bw;
-        mNotification.flags |= Notification.FLAG_ONGOING_EVENT;
-        mNotification.setLatestEventInfo(getApplicationContext(), title, artist, pi);
+        // Build a virtual task stack
+        final Intent musicPlayerActivity = new Intent(getApplicationContext(), MainMenuActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainMenuActivity.class);
+        stackBuilder.addNextIntent(musicPlayerActivity);
+
+        // Prepare the notification
+        final RemoteViews contentView;
+        if (mNotification == null || mNotification.contentView == null) {
+            contentView = new RemoteViews(getPackageName(), R.layout.notification);
+        } else {
+            contentView = mNotification.contentView;
+        }
+        contentView.setTextViewText(R.id.notificationTitle, mCurrentMusic.getTitle());
+        contentView.setTextViewText(R.id.notificationArtist, mCurrentMusic.getArtist());
+
+        // Build notification actions
+        final Intent playPause = new Intent(this, MusicService.class);
+        playPause.setAction(MusicService.ACTION_TOGGLE_PLAYBACK);
+        final Intent next = new Intent(this, MusicService.class);
+        next.setAction(MusicService.ACTION_SKIP);
+        final Intent quit = new Intent(this, MusicService.class);
+        quit.setAction(MusicService.ACTION_STOP);
+
+        contentView.setOnClickPendingIntent(R.id.notificationPlayPause, PendingIntent.getService(this, 0, playPause, 0));
+        contentView.setOnClickPendingIntent(R.id.notificationNext, PendingIntent.getService(this, 0, next, 0));
+        contentView.setOnClickPendingIntent(R.id.notificationClose, PendingIntent.getService(this, 0, quit, 0));
+        PendingIntent pi = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Set notification play/pause icon state
+        contentView.setImageViewResource(R.id.notificationPlayPause, state == RemoteControlClient.PLAYSTATE_PLAYING ? R.drawable.ic_media_play : R.drawable.ic_media_pause);
+
+        // Finish the notification
+        if (mNotification == null) {
+            final Notification.Builder builder = new Notification.Builder(this);
+            builder.setSmallIcon(R.drawable.icon_bw);
+            builder.setContentIntent(pi);
+            builder.setContent(contentView);
+            mNotification = builder.build();
+        }
+
+        mNotificationManager.notify(NOTIFICATION_ID, mNotification);
         startForeground(NOTIFICATION_ID, mNotification);
     }
 
