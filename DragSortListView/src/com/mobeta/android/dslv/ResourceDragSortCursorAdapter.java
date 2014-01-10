cdb1506483d6cb1ddd12864d1d37f470fb968294
Avuton Olrich <avuton@gmail.com>
2014-01-10 13:10:42 -0800

    Remove depreciated DSLV constructors.
    Since DragSortListView is unmaintained now, it's good to just go ahead and
    remove the depreciated (and unused) constructors which emit useless warnings
    rather than even supress them.

--- DragSortListView/src/com/mobeta/android/dslv/ResourceDragSortCursorAdapter.java
+++ DragSortListView/src/com/mobeta/android/dslv/ResourceDragSortCursorAdapter.java
@@ -37,27 +37,7 @@ public abstract class ResourceDragSortCursorAdapter extends DragSortCursorAdapte
     private int mDropDownLayout;
     
     private LayoutInflater mInflater;
-    
-    /**
-     * Constructor the enables auto-requery.
-     *
-     * @deprecated This option is discouraged, as it results in Cursor queries
-     * being performed on the application's UI thread and thus can cause poor
-     * responsiveness or even Application Not Responding errors.  As an alternative,
-     * use {@link android.app.LoaderManager} with a {@link android.content.CursorLoader}.
-     *
-     * @param context The context where the ListView associated with this adapter is running
-     * @param layout resource identifier of a layout file that defines the views
-     *            for this list item.  Unless you override them later, this will
-     *            define both the item views and the drop down views.
-     */
-    @Deprecated
-    public ResourceDragSortCursorAdapter(Context context, int layout, Cursor c) {
-        super(context, c);
-        mLayout = mDropDownLayout = layout;
-        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
-    
+
     /**
      * Constructor with default behavior as per
      * {@link CursorAdapter#CursorAdapter(Context, Cursor, boolean)}; it is recommended
