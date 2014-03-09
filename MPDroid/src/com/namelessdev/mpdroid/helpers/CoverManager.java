842d4e82f6ec5b9abf8e4e51efb80c76ceac7214
Avuton Olrich <avuton@gmail.com>
2014-03-09 14:33:58 -0700

    CoverManager: Properly close resources after use.

--- MPDroid/src/com/namelessdev/mpdroid/helpers/CoverManager.java
+++ MPDroid/src/com/namelessdev/mpdroid/helpers/CoverManager.java
@@ -624,8 +624,8 @@ public class CoverManager {
 
         URL url = buildURLForConnection(textUrl);
         HttpURLConnection connection = getHttpConnection(url);
-        BufferedInputStream bis;
-        ByteArrayOutputStream baos;
+        BufferedInputStream bis = null;
+        ByteArrayOutputStream baos = null;
         byte[] buffer = null;
         int len;
 
@@ -633,6 +633,7 @@ public class CoverManager {
             return null;
         }
 
+        /** TODO: After minSdkVersion="19" use try-with-resources here. */
         try {
             bis = new BufferedInputStream(connection.getInputStream(), 8192);
             baos = new ByteArrayOutputStream();
@@ -645,6 +646,24 @@ public class CoverManager {
         } catch (Exception e) {
             e(CoverAsyncHelper.class.getSimpleName(), "Failed to download cover :" + e);
         } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    Log.e(CoverAsyncHelper.class.getSimpleName(),
+                            "Failed to close the BufferedInputStream.", e);
+                }
+            }
+
+            if (baos != null) {
+                try {
+                    baos.close();
+                } catch (IOException e) {
+                    Log.e(MPDApplication.TAG,
+                            "Failed to close the BufferedArrayOutputStream.", e);
+                }
+            }
+
             if (connection != null) {
                 connection.disconnect();
             }
