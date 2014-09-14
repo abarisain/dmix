b9c7f1a550248a12b5aac63c487066810a23c2bb
Avuton Olrich <avuton@gmail.com>
2014-09-13 18:49:17 -0700

    Item: Clean up warnings

--- JMPDComm/src/main/java/org/a0z/mpd/item/Item.java
+++ JMPDComm/src/main/java/org/a0z/mpd/item/Item.java
@@ -34,43 +34,47 @@ import java.util.Locale;
 
 public abstract class Item implements Comparable<Item> {
 
-    public static final Collator defaultCollator = Collator.getInstance(Locale.getDefault());
+    public static final Collator DEFAULT_COLLATOR = Collator.getInstance(Locale.getDefault());
 
     /*
-     * Merge item lists, for example received by albumartist and artist
+     * Merge item lists, for example received by album artist and artist
      * requests. Sorted lists required!
      */
-    public static <T extends Item> List<T> merged(List<T> aa_items,
-            List<T> a_items) {
-        int j_start = aa_items.size() - 1;
-        for (int i = a_items.size() - 1; i >= 0; i--) { // artists
+    public static <T extends Item> List<T> merged(final List<T> albumArtists,
+            final List<T> artists) {
+        int j_start = albumArtists.size() - 1;
+        for (int i = artists.size() - 1; i >= 0; i--) { // artists
             for (int j = j_start; j >= 0; j--) { // album artists
-                if (aa_items.get(j).nameEquals(a_items.get(i))) {
+                if (albumArtists.get(j).nameEquals(artists.get(i))) {
                     j_start = j;
-                    a_items.remove(i);
+                    artists.remove(i);
                     break;
                 }
             }
         }
-        a_items.addAll(aa_items);
-        Collections.sort(a_items);
-        return a_items;
+        artists.addAll(albumArtists);
+        Collections.sort(artists);
+        return artists;
     }
 
     @Override
-    public int compareTo(Item o) {
+    public int compareTo(final Item another) {
+        final int comparisonResult;
+
         // sort "" behind everything else
         if (sortText() != null && sortText().isEmpty()) {
-            if (o.sortText() != null && o.sortText().isEmpty()) {
-                return 0;
+            if (another.sortText() != null && another.sortText().isEmpty()) {
+                comparisonResult = 0;
+            } else {
+                comparisonResult = 1;
             }
-            return 1;
+        } else if (another.sortText() != null && another.sortText().isEmpty()) {
+            comparisonResult = -1;
+        } else {
+            comparisonResult = DEFAULT_COLLATOR.compare(sortText(), another.sortText());
         }
-        if (o.sortText() != null && o.sortText().isEmpty()) {
-            return -1;
-        }
-        return defaultCollator.compare(sortText(), o.sortText());
-        // return sort().compareToIgnoreCase(o.sort());
+
+        return comparisonResult;
     }
 
     public abstract String getName();
@@ -87,7 +91,7 @@ public abstract class Item implements Comparable<Item> {
         return getName();
     }
 
-    public boolean nameEquals(Item o) {
+    public boolean nameEquals(final Item o) {
         return getName().equals(o.getName());
     }
 
