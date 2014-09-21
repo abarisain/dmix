3765ed0e3aad14623d354aefe8a3128b43f6413d
Avuton Olrich <avuton@gmail.com>
2014-09-21 15:34:58 -0700

    MPD: Begin cleaning up getFirstTrack().

--- JMPDComm/src/main/java/org/a0z/mpd/MPD.java
+++ JMPDComm/src/main/java/org/a0z/mpd/MPD.java
@@ -758,12 +758,23 @@ public class MPD {
         return mpdConnection.getHostPort();
     }
 
-    protected List<Music> getFirstTrack(Album album) throws MPDServerException {
-        Artist artist = album.getArtist();
-        String[] args = new String[6];
-        args[0] = (artist == null ? "" : album.hasAlbumArtist() ? MPDCommand.MPD_TAG_ALBUM_ARTIST
-                : MPDCommand.MPD_TAG_ARTIST);
-        args[1] = (artist == null ? "" : artist.getName());
+    protected List<Music> getFirstTrack(final Album album) throws MPDServerException {
+        final Artist artist = album.getArtist();
+        final String[] args = new String[6];
+
+        if (artist == null) {
+            args[0] = "";
+            args[1] = "";
+        } else if (album.hasAlbumArtist()) {
+            args[0] = MPDCommand.MPD_TAG_ALBUM_ARTIST;
+        } else {
+            args[0] = MPDCommand.MPD_TAG_ARTIST;
+        }
+
+        if (artist != null) {
+            args[1] = artist.getName();
+        }
+
         args[2] = MPDCommand.MPD_TAG_ALBUM;
         args[3] = album.getName();
         args[4] = "track";
