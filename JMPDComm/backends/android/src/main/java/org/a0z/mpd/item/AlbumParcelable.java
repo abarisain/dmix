abarisain/dmix

commit 162259d4ae45ad6070b341bbf2324321b4df6d05

Album: Order signatures the same way.

JMPDComm/backends/android/src/main/java/org/a0z/mpd/item/AlbumParcelable.java
@@ -51,11 +51,11 @@ public class AlbumParcelable extends Album implements Parcelable {
 
     protected AlbumParcelable(final Parcel in) {
         super(in.readString(), /** name */
+                new Artist(in.readString()), /** artist */
+                in.readInt() > 0, /** hasAlbumArtist */
                 in.readLong(), /** songCount */
                 in.readLong(), /** duration */
                 in.readLong(), /** year */
-                new Artist(in.readString()), /** artist */
-                in.readInt() > 0, /** hasAlbumArtist */
                 in.readString()); /** path */
     }
 

JMPDComm/src/main/java/org/a0z/mpd/item/Album.java
@@ -47,20 +47,20 @@ public class Album extends Item {
     private boolean hasAlbumArtist;
 
     public Album(Album a) {
-        this(a.name, a.songCount, a.duration, a.year,
-                new Artist(a.artist), a.hasAlbumArtist, a.path);
+        this(a.name, new Artist(a.artist), a.hasAlbumArtist, a.songCount, a.duration, a.year,
+                a.path);
     }
 
     public Album(String name, Artist artist) {
-        this(name, 0L, 0L, 0L, artist, false, "");
+        this(name, artist, false, 0L, 0L, 0L, "");
     }
 
     public Album(String name, Artist artist, boolean hasAlbumArtist) {
-        this(name, 0L, 0L, 0L, artist, hasAlbumArtist, "");
+        this(name, artist, hasAlbumArtist, 0L, 0L, 0L, "");
     }
 
-    public Album(String name, long songCount, long duration, long year,
-            Artist artist, boolean hasAlbumArtist, String path) {
+    public Album(String name, Artist artist, boolean hasAlbumArtist, long songCount, long duration,
+            long year, String path) {
         this.name = name;
         this.songCount = songCount;
         this.duration = duration;

commit a0b33015bc548b6bcbd1138f78fc2aa820a89296

AlbumParcelable: Fix write ordering.
This fixes a regression from 162259d.

JMPDComm/backends/android/src/main/java/org/a0z/mpd/item/AlbumParcelable.java
@@ -76,11 +76,11 @@ public class AlbumParcelable extends Album implements Parcelable {
         }
 
         dest.writeString(getName());
+        dest.writeString(artistName);
+        dest.writeInt(hasAlbumArtist() ? 1 : 0);
         dest.writeLong(getSongCount());
         dest.writeLong(getDuration());
         dest.writeLong(getYear());
-        dest.writeString(artistName);
-        dest.writeInt(hasAlbumArtist() ? 1 : 0);
         dest.writeString(getPath());
     }
 }