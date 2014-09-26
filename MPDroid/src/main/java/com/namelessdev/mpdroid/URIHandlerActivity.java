abarisain/dmix

commit 00055d6ae4670178aa97a01caf87aa41f2504e03

URIHandlerActivity: Warning cleanup.

MPDroid/src/main/java/com/namelessdev/mpdroid/URIHandlerActivity.java
@@ -16,23 +16,29 @@
 
 package com.namelessdev.mpdroid;
 
+import com.namelessdev.mpdroid.fragments.StreamsFragment;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-
-import com.namelessdev.mpdroid.fragments.StreamsFragment;
+import android.support.v4.app.FragmentManager;
 
 public class URIHandlerActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Intent intent = getIntent();
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final StreamsFragment streamsFragment =
+                (StreamsFragment) fragmentManager.findFragmentById(R.id.streamsFragment);
+
         setContentView(R.layout.activity_urihandler);
-        if (!getIntent().getAction().equals("android.intent.action.VIEW")) {
+        if (!Intent.ACTION_VIEW.equals(intent.getAction())) {
             finish();
         }
-        final StreamsFragment sf = (StreamsFragment) getSupportFragmentManager().findFragmentById(
-                R.id.streamsFragment);
-        sf.addEdit(-1, getIntent().getDataString());
+
+        streamsFragment.addEdit(-1, intent.getDataString());
     }
 
 }

commit 91ad464413571ca9be9845929d15496cc8eaa7cc

URIHandlerActivity: Set the content view prior to trying to find the fragment.
This fixes a regression from 00055d6.

MPDroid/src/main/java/com/namelessdev/mpdroid/URIHandlerActivity.java
@@ -28,12 +28,13 @@ public class URIHandlerActivity extends FragmentActivity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_urihandler);
+
         final Intent intent = getIntent();
         final FragmentManager fragmentManager = getSupportFragmentManager();
         final StreamsFragment streamsFragment =
                 (StreamsFragment) fragmentManager.findFragmentById(R.id.streamsFragment);
 
-        setContentView(R.layout.activity_urihandler);
         if (!Intent.ACTION_VIEW.equals(intent.getAction())) {
             finish();
         }