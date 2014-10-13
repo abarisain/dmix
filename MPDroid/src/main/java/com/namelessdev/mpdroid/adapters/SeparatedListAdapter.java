03927ffe79534c456e88276252f0c686fd0acf80
Avuton Olrich <avuton@gmail.com>
2014-10-13 09:49:38 -0700

    SeparatedListAdapter: Cleanup warnings.

--- MPDroid/src/main/java/com/namelessdev/mpdroid/adapters/SeparatedListAdapter.java
+++ MPDroid/src/main/java/com/namelessdev/mpdroid/adapters/SeparatedListAdapter.java
@@ -26,6 +26,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -45,15 +46,15 @@ public class SeparatedListAdapter extends BaseAdapter {
 
     private static final int TYPE_SEPARATOR = 1;
 
-    private SeparatedListDataBinder mBinder; // The content -> view 'binding'
+    private final SeparatedListDataBinder mBinder; // The content -> view 'binding'
 
-    private Context mContext;
+    private final Context mContext;
 
-    private LayoutInflater mInflater;
+    private final LayoutInflater mInflater;
 
-    private List<?> mItems; // Content
+    private final List<?> mItems; // Content
 
-    private int mSeparatorLayoutId;
+    private final int mSeparatorLayoutId;
 
     private int mViewId = -1; // The view to be displayed
 
@@ -61,14 +62,18 @@ public class SeparatedListAdapter extends BaseAdapter {
             @LayoutRes final int separatorViewId, final SeparatedListDataBinder binder,
             final List<?> items) {
         super();
-        init(context, viewId, separatorViewId, binder, items);
+        mViewId = viewId;
+        mBinder = binder;
+        mItems = Collections.unmodifiableList(items);
+        mContext = context;
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mSeparatorLayoutId = separatorViewId;
     }
 
     public SeparatedListAdapter(
             final Context context, @LayoutRes final int viewId,
             final SeparatedListDataBinder binder, final List<?> items) {
-        super();
-        init(context, viewId, R.layout.list_separator, binder, items);
+        this(context, viewId, R.layout.list_separator, binder, items);
     }
 
     @Override
@@ -88,53 +93,51 @@ public class SeparatedListAdapter extends BaseAdapter {
 
     @Override
     public long getItemId(final int position) {
-        return position;
+        return (long) position;
     }
 
     @Override
     public int getItemViewType(final int position) {
+        final int viewType;
+
         if (mItems.get(position) instanceof String) {
-            return TYPE_SEPARATOR;
+            viewType = TYPE_SEPARATOR;
+        } else {
+            viewType = TYPE_CONTENT;
         }
-        return TYPE_CONTENT;
+
+        return viewType;
     }
 
     @Override
-    public View getView(final int position, View convertView, final ViewGroup parent) {
+    public View getView(final int position, final View convertView, final ViewGroup parent) {
         final int itemType = getItemViewType(position);
+        final View view;
 
         if (convertView == null) {
             if (itemType == TYPE_SEPARATOR) {
-                convertView = mInflater.inflate(mSeparatorLayoutId, parent, false);
+                view = mInflater.inflate(mSeparatorLayoutId, parent, false);
             } else {
-                convertView = mInflater.inflate(mViewId, parent, false);
+                view = mInflater.inflate(mViewId, parent, false);
             }
+        } else {
+            view = convertView;
         }
 
         if (itemType == TYPE_SEPARATOR) {
             final CharSequence separator = (CharSequence) mItems.get(position);
 
-            ((TextView) convertView.findViewById(R.id.separator_title)).setText(separator);
+            ((TextView) view.findViewById(R.id.separator_title)).setText(separator);
         } else {
-            mBinder.onDataBind(mContext, convertView, mItems, mItems.get(position), position);
+            mBinder.onDataBind(mContext, view, mItems, mItems.get(position), position);
         }
 
-        return convertView;
+        return view;
     }
 
     @Override
     public int getViewTypeCount() {
         return 2;
-    }
-
-    private void init(final Context context, final int viewId, final int separatorViewId,
-            final SeparatedListDataBinder binder, final List<?> items) {
-        mViewId = viewId;
-        mBinder = binder;
-        mItems = items;
-        mContext = context;
-        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        mSeparatorLayoutId = separatorViewId;
     }
 
     @Override
