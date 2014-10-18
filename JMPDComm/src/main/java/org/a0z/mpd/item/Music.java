abarisain/dmix

commit ba1abd7191d133f4a5f4a0ba9e97b4c105e7c4e2

Remove # and ${hash} hack

JMPDComm/src/main/java/org/a0z/mpd/item/Music.java
@@ -523,7 +523,6 @@ public class Music extends Item implements FilesystemTreeEntry {
             int pos = fullpath.indexOf("#");
             if (pos > 1) {
                 name=fullpath.substring(pos + 1, fullpath.length());
-                name=name.replace("${hash}", "#");
                 fullpath=fullpath.substring(0, pos-1);
             }
         }
@@ -533,15 +532,14 @@ public class Music extends Item implements FilesystemTreeEntry {
         if (null == name || name.isEmpty()) {
             return url;
         }
-        String fixed = name.replace("#", "${hash}");
         try {
             String path = new URL(url).getPath();
             if (null == path || path.isEmpty()) {
-                return url + "/#" + fixed;
+                return url + "/#" + name;
             }
-        } catch (final MalformedURLException ignored) {
+        } catch (MalformedURLException e) {
         }
-        return url + "#" + fixed;
+        return url + "#" + name;
     }
 
     /**

commit c16a3e3b1bdaf05a0f2227429301f3dc00dd776a

Music: Fix one off regression from ba1abd7.
This caused stream extensions to get chopped by one character.

JMPDComm/src/main/java/org/a0z/mpd/item/Music.java
@@ -168,7 +168,7 @@ public class Music extends Item implements FilesystemTreeEntry {
                         final int pos = fullPath.indexOf('#');
                         if (pos > 1) {
                             name = fullPath.substring(pos + 1, fullPath.length());
-                            fullPath = fullPath.substring(0, pos - 1);
+                            fullPath = fullPath.substring(0, pos);
                         }
                     }
                     break;