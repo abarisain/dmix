abarisain/dmix

commit 51cecb79f01302138b1fccdfd644ac7c90c776f6

dmix: Implement unified equals/hashcode where overridden.
This patch adds a more readable Object.equals(object) implementation and
Object.hashCode(). This commit also adds hashCode() where it is missing.
This commit ensures the internal objects for computing hashCode() are final.
This commit removes equals()/hashCode() from CoverInfo, it's duplicative
of AlbumInfo. Finally, this moves StringsUtils to Tools.

JMPDComm/src/main/java/org/a0z/mpd/MPD.java
@@ -552,8 +552,8 @@ public class MPD {
                 Arrays.sort(aartists); // make sure "" is the first one
                 if (aartists[0] != null && !aartists[0].isEmpty()) { // one albumartist, fix this
                     // album
-                    a.setArtist(new Artist(aartists[0]));
-                    a.setHasAlbumArtist(true);
+                    final Artist artist = new Artist(aartists[0]);
+                    a = a.setArtist(artist);
                 } // do nothing if albumartist is ""
                 if (aartists.length > 1) { // it's more than one album, insert
                     for (int n = 1; n < aartists.length; n++) {

JMPDComm/src/main/java/org/a0z/mpd/AlbumInfo.java
@@ -30,22 +30,21 @@ package org.a0z.mpd;
 import org.a0z.mpd.item.Album;
 import org.a0z.mpd.item.Artist;
 
-import static org.a0z.mpd.StringsUtils.getHashFromString;
+import java.util.Arrays;
+
+import static org.a0z.mpd.Tools.getHashFromString;
 
 public class AlbumInfo {
 
-    protected String artist = "";
+    protected final String artist;
 
-    protected String album = "";
+    protected final String album;
 
-    protected String path = "";
+    protected String path;
 
-    protected String filename = "";
+    protected String filename;
 
     private static final String INVALID_ALBUM_KEY = "INVALID_ALBUM_KEY";
-
-    public AlbumInfo() {
-    }
 
     public AlbumInfo(Album album) {
         Artist a = album.getArtist();
@@ -54,11 +53,8 @@ public class AlbumInfo {
         this.path = album.getPath();
     }
 
-    public AlbumInfo(AlbumInfo a) {
-        this.artist = a.artist;
-        this.album = a.album;
-        this.path = a.path;
-        this.filename = a.filename;
+    public AlbumInfo(AlbumInfo albumInfo) {
+        this(albumInfo.artist, albumInfo.album, albumInfo.path, albumInfo.filename);
     }
 
     public AlbumInfo(String artist, String album) {
@@ -74,24 +70,32 @@ public class AlbumInfo {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(final Object o) {
+        Boolean isEqual = null;
+
         if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        AlbumInfo albumInfo = (AlbumInfo) o;
-
-        if (album != null ? !album.equals(albumInfo.album) : albumInfo.album != null) {
-            return false;
-        }
-        if (artist != null ? !artist.equals(albumInfo.artist) : albumInfo.artist != null) {
-            return false;
+            isEqual = Boolean.TRUE;
+        } else if (o == null || getClass() != o.getClass()) {
+            isEqual = Boolean.FALSE;
         }
 
-        return true;
+        if (isEqual == null || isEqual.equals(Boolean.TRUE)) {
+            final AlbumInfo albumInfo = (AlbumInfo) o;
+
+            if (Tools.isNotEqual(album, albumInfo.album)) {
+                isEqual = Boolean.FALSE;
+            }
+
+            if (Tools.isNotEqual(artist, albumInfo.artist)) {
+                isEqual = Boolean.FALSE;
+            }
+        }
+
+        if (isEqual == null) {
+            isEqual = Boolean.TRUE;
+        }
+
+        return isEqual.booleanValue();
     }
 
     public String getAlbum() {
@@ -116,23 +120,13 @@ public class AlbumInfo {
 
     @Override
     public int hashCode() {
-        int result = artist != null ? artist.hashCode() : 0;
-        result = 31 * result + (album != null ? album.hashCode() : 0);
-        return result;
+        return Arrays.hashCode(new Object[] {artist, album});
     }
 
     public boolean isValid() {
         final boolean isArtistEmpty = artist == null || artist.isEmpty();
         final boolean isAlbumEmpty = album == null || album.isEmpty();
         return !isAlbumEmpty && !isArtistEmpty;
-    }
-
-    public void setAlbum(String album) {
-        this.album = album;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
     }
 
     public void setFilename(String filename) {

JMPDComm/src/main/java/org/a0z/mpd/MPDStatistics.java
@@ -151,7 +151,7 @@ public class MPDStatistics {
      */
     public final void update(final Iterable<String> response) {
         for (final String line : response) {
-            final String[] lines = StringsUtils.MPD_DELIMITER.split(line, 2);
+            final String[] lines = Tools.MPD_DELIMITER.split(line, 2);
 
             switch (lines[0]) {
                 case "albums":

JMPDComm/src/main/java/org/a0z/mpd/MPDStatus.java
@@ -434,7 +434,7 @@ public class MPDStatus {
         resetValues();
 
         for (final String line : response) {
-            final String[] lines = StringsUtils.MPD_DELIMITER.split(line, 2);
+            final String[] lines = Tools.MPD_DELIMITER.split(line, 2);
 
             switch (lines[0]) {
                 case "audio":

JMPDComm/src/main/java/org/a0z/mpd/Tools.java
@@ -30,7 +30,7 @@ package org.a0z.mpd;
 import java.security.MessageDigest;
 import java.util.regex.Pattern;
 
-public final class StringsUtils {
+public final class Tools {
 
     /**
      * This is a regular expression pattern matcher for the typical MPD protocol response.
@@ -39,7 +39,7 @@ public final class StringsUtils {
 
     private static final Pattern EXTENSION_DELIMITER = Pattern.compile("\\.");
 
-    private StringsUtils() {
+    private Tools() {
         super();
     }
 
@@ -80,6 +80,33 @@ public final class StringsUtils {
             }
         }
         return "";
+    }
+
+    /**
+     * Compares inside objects for an Object.equals(object) implementation.
+     *
+     * @param objectA An object to be compared.
+     * @param objectB An object to be compared.
+     * @return False if objects are both null or are equal, true otherwise.
+     */
+    public static boolean isNotEqual(final Object objectA, final Object objectB) {
+        final boolean isEqual;
+
+        if (objectA == null) {
+            if (objectB == null) {
+                isEqual = true;
+            } else {
+                isEqual = false;
+            }
+        } else {
+            if (objectA.equals(objectB)) {
+                isEqual = true;
+            } else {
+                isEqual = false;
+            }
+        }
+
+        return !isEqual;
     }
 
     /**

JMPDComm/src/main/java/org/a0z/mpd/item/Album.java
@@ -29,10 +29,13 @@ package org.a0z.mpd.item;
 
 import org.a0z.mpd.AlbumInfo;
 import org.a0z.mpd.MPD;
+import org.a0z.mpd.Tools;
+
+import java.util.Arrays;
 
 public class Album extends Item {
 
-    private String name;
+    private final String name;
 
     private long songCount;
 
@@ -42,7 +45,7 @@ public class Album extends Item {
 
     private String path;
 
-    private Artist artist;
+    private final Artist artist;
 
     private boolean hasAlbumArtist;
 
@@ -52,11 +55,11 @@ public class Album extends Item {
     }
 
     public Album(String name, Artist artist) {
-        this(name, artist, false, 0L, 0L, 0L, "");
+        this(name, artist, false, 0L, 0L, 0L, null);
     }
 
     public Album(String name, Artist artist, boolean hasAlbumArtist) {
-        this(name, artist, hasAlbumArtist, 0L, 0L, 0L, "");
+        this(name, artist, hasAlbumArtist, 0L, 0L, 0L, null);
     }
 
     public Album(String name, Artist artist, boolean hasAlbumArtist, long songCount, long duration,
@@ -79,23 +82,54 @@ public class Album extends Item {
                     return year < oa.year ? -1 : 1;
                 }
             }
-            //int comp = super.compareTo(o);
-            // if (comp == 0 && artist != null) { // same album name, check artist
-            //    comp = artist.compareTo(oa.artist);
-            // }
         }
         return super.compareTo(o);
     }
 
-
+    /**
+     * Compares an Artist object with a general contract of
+     * comparison that is reflexive, symmetric and transitive.
+     *
+     * @param o The object to compare this instance with.
+     * @return True if the objects are equal with regard to te general contract, false otherwise.
+     * @see Object#equals(Object)
+     */
     @Override
-    public boolean equals(Object o) {
-        if (o instanceof Album) {
-            Album a = (Album) o;
-            return (hasAlbumArtist == a.hasAlbumArtist &&
-                    name.equals(a.getName()) && artist.equals(a.getArtist()));
+    public boolean equals(final Object o) {
+        Boolean isEqual = null;
+
+        if (this == o) {
+            isEqual = Boolean.TRUE;
+        } else if (o == null || getClass() != o.getClass()) {
+            isEqual = Boolean.FALSE;
         }
-        return false;
+
+        if (isEqual == null || isEqual.equals(Boolean.TRUE)) {
+            final Album album = (Album) o;
+
+            if (Tools.isNotEqual(name, album.name) || Tools.isNotEqual(artist, album.artist)) {
+                isEqual = Boolean.FALSE;
+            }
+        }
+
+        if (isEqual == null) {
+            isEqual = Boolean.TRUE;
+        }
+
+        return isEqual.booleanValue();
+    }
+
+    /**
+     * Returns an integer hash code for this Artist. By contract, any two objects for which
+     * {@link #equals} returns {@code true} must return the same hash code value. This means that
+     * subclasses of {@code Object} usually override both methods or neither method.
+     *
+     * @return This Artist hash code.
+     * @see Object#equals(Object)
+     */
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(new Object[]{name, artist});
     }
 
     public AlbumInfo getAlbumInfo() {
@@ -155,8 +189,15 @@ public class Album extends Item {
         return false;
     }
 
-    public void setArtist(Artist artist) {
-        this.artist = artist;
+    /**
+     * This sets the artist in a new object, due to the required immutability of name
+     * and artist to satisfy the requirement that the hash code not change over time.
+     *
+     * @param artist The new artist.
+     * @return A new Album object based off this Album object.
+     */
+    public Album setArtist(final Artist artist) {
+        return new Album(name, artist, hasAlbumArtist, songCount, duration, year, path);
     }
 
     public void setDuration(long d) {

JMPDComm/src/main/java/org/a0z/mpd/item/Artist.java
@@ -28,14 +28,16 @@
 package org.a0z.mpd.item;
 
 import org.a0z.mpd.MPD;
+import org.a0z.mpd.Tools;
 
+import java.util.Arrays;
 import java.util.Locale;
 
 public class Artist extends Item {
 
-    private String name;
+    private final String name;
 
-    private String sort;
+    private final String sort;
 
     public Artist(Artist a) {
         this(a.name, a.sort);
@@ -57,9 +59,50 @@ public class Artist extends Item {
         this.sort = sort;
     }
 
+    /**
+     * Compares an Artist object with a general contract of
+     * comparison that is reflexive, symmetric and transitive.
+     *
+     * @param o The object to compare this instance with.
+     * @return True if the objects are equal with regard to te general contract, false otherwise.
+     * @see Object#equals(Object)
+     */
     @Override
-    public boolean equals(Object o) {
-        return (o instanceof Artist) && ((Artist) o).name.equals(name);
+    public boolean equals(final Object o) {
+        Boolean isEqual = null;
+
+        if (this == o) {
+            isEqual = Boolean.TRUE;
+        } else if (o == null || getClass() != o.getClass()) {
+            isEqual = Boolean.FALSE;
+        }
+
+        if (isEqual == null || isEqual.equals(Boolean.TRUE)) {
+            final Artist artist = (Artist) o;
+
+            if (Tools.isNotEqual(name, artist.name) || Tools.isNotEqual(sort, artist.sort)) {
+                isEqual = Boolean.FALSE;
+            }
+        }
+
+        if (isEqual == null) {
+            isEqual = Boolean.TRUE;
+        }
+
+        return isEqual.booleanValue();
+    }
+
+    /**
+     * Returns an integer hash code for this Artist. By contract, any two objects for which
+     * {@link #equals} returns {@code true} must return the same hash code value. This means that
+     * subclasses of {@code Object} usually override both methods or neither method.
+     *
+     * @return This Artist hash code.
+     * @see Object#equals(Object)
+     */
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(new Object[]{name, sort});
     }
 
     public String getName() {

JMPDComm/src/main/java/org/a0z/mpd/item/Directory.java
@@ -28,7 +28,7 @@
 package org.a0z.mpd.item;
 
 import org.a0z.mpd.MPD;
-import org.a0z.mpd.StringsUtils;
+import org.a0z.mpd.Tools;
 import org.a0z.mpd.exception.InvalidParameterException;
 import org.a0z.mpd.exception.MPDServerException;
 
@@ -144,7 +144,7 @@ public final class Directory extends Item implements FilesystemTreeEntry {
 
             // If we hit anything we know is an item, consume the linecache
             final String line = response.get(i);
-            final String[] lines = StringsUtils.MPD_DELIMITER.split(line, 2);
+            final String[] lines = Tools.MPD_DELIMITER.split(line, 2);
 
             switch (lines[0]) {
                 case "directory":

commit 53a74be875a123669e406d6c75275d230fda81a6

MPD: Fix Album Artist setting regression from 51cecb7.
Second attempt to fix this one.

JMPDComm/src/main/java/org/a0z/mpd/MPD.java
@@ -1303,13 +1303,13 @@ public class MPD {
                 // Don't make the check with the other so we don't waste time doing string
                 // comparisons for nothing.
                 if (currentAlbum != null) {
-                    currentAlbum.setAlbumArtist(new Artist(pair[VALUE]));
+                    final Album newAlbum = currentAlbum.setAlbumArtist(new Artist(pair[VALUE]));
+                    result.add(newAlbum);
                 }
             } else if (albumResponse.equals(pair[KEY])) {
                 if (!pair[VALUE].isEmpty() || includeUnknownAlbum) {
                     currentAlbum = new Album(pair[VALUE], null);
                     currentAlbum.setHasAlbumArtist(useAlbumArtist);
-                    result.add(currentAlbum);
                 } else {
                     currentAlbum = null;
                 }