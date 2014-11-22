abarisain/dmix

commit 4b7c035c0003174f6cf5233516e054acd69152ba

MPD: Fix (very) slow add.
This fixes #635 using MPD's commands, if available.

JMPDComm/src/main/java/org/a0z/mpd/MPD.java
@@ -213,8 +213,17 @@ public class MPD {
      */
     public void add(final Album album, final boolean replace, final boolean play)
             throws IOException, MPDException {
-        final List<Music> songs = getSongs(album);
-        final CommandQueue commandQueue = MPDPlaylist.addAllCommand(songs);
+        final CommandQueue commandQueue;
+
+        if (isCommandAvailable(MPDCommand.MPD_CMD_FIND_ADD)) {
+            commandQueue = new CommandQueue();
+
+            commandQueue
+                    .add(MPDCommand.MPD_CMD_FIND_ADD, MPDCommand.MPD_FIND_ALBUM, album.getName());
+        } else {
+            final List<Music> songs = getSongs(album);
+            commandQueue = MPDPlaylist.addAllCommand(songs);
+        }
 
         add(commandQueue, replace, play);
     }
@@ -241,8 +250,17 @@ public class MPD {
      */
     public void add(final Artist artist, final boolean replace, final boolean play)
             throws IOException, MPDException {
-        final List<Music> songs = getSongs(artist);
-        final CommandQueue commandQueue = MPDPlaylist.addAllCommand(songs);
+        final CommandQueue commandQueue;
+
+        if (isCommandAvailable(MPDCommand.MPD_CMD_FIND_ADD)) {
+            commandQueue = new CommandQueue();
+
+            commandQueue
+                    .add(MPDCommand.MPD_CMD_FIND_ADD, MPDCommand.MPD_FIND_ARTIST, artist.getName());
+        } else {
+            final List<Music> songs = getSongs(artist);
+            commandQueue = MPDPlaylist.addAllCommand(songs);
+        }
 
         add(commandQueue, replace, play);
     }
@@ -394,12 +412,22 @@ public class MPD {
 
     public void addToPlaylist(final String playlistName, final Album album)
             throws IOException, MPDException {
-        addToPlaylist(playlistName, new ArrayList<>(getSongs(album)));
+        if (mIdleConnection.isCommandAvailable(MPDCommand.MPD_CMD_SEARCH_ADD_PLAYLIST)) {
+            mConnection.sendCommand(MPDCommand.MPD_CMD_SEARCH_ADD_PLAYLIST, playlistName,
+                    MPDCommand.MPD_SEARCH_ALBUM, album.getName());
+        } else {
+            addToPlaylist(playlistName, new ArrayList<>(getSongs(album)));
+        }
     }
 
     public void addToPlaylist(final String playlistName, final Artist artist)
             throws IOException, MPDException {
-        addToPlaylist(playlistName, new ArrayList<>(getSongs(artist)));
+        if (mIdleConnection.isCommandAvailable(MPDCommand.MPD_CMD_SEARCH_ADD_PLAYLIST)) {
+            mConnection.sendCommand(MPDCommand.MPD_CMD_SEARCH_ADD_PLAYLIST, playlistName,
+                    MPDCommand.MPD_SEARCH_ARTIST, artist.getName());
+        } else {
+            addToPlaylist(playlistName, new ArrayList<>(getSongs(artist)));
+        }
     }
 
     public void addToPlaylist(final String playlistName, final Collection<Music> musicCollection)

JMPDComm/src/main/java/org/a0z/mpd/MPDCommand.java
@@ -56,6 +56,9 @@ public class MPDCommand {
 
     public static final String MPD_CMD_FIND = "find";
 
+    /** Added in MPD protocol 0.16.0 */
+    public static final String MPD_CMD_FIND_ADD = "findadd";
+
     public static final String MPD_CMD_GROUP = "group";
 
     public static final String MPD_CMD_IDLE = "idle";
@@ -114,6 +117,9 @@ public class MPDCommand {
     public static final String MPD_CMD_REPEAT = "repeat";
 
     public static final String MPD_CMD_SEARCH = "search";
+
+    /** Added in MPD protocol 0.17.0. */
+    public static final String MPD_CMD_SEARCH_ADD_PLAYLIST = "searchaddpl";
 
     public static final String MPD_CMD_SEEK = "seek";
 

commit 1a1478a84d110a39c28f1d30f46b57cb5daa8cf4

MPD: Make album adding a bit more greedy.
This fixes a regression with 4b7c035 where the album would be added without an
artist being specified. This also adds a helper method, no documentation as
this code is likely to be rewritten prior to next release.

JMPDComm/src/main/java/org/a0z/mpd/MPD.java
@@ -113,44 +113,36 @@ public class MPD {
         connect(server, port, password);
     }
 
-    private static MPDCommand getAlbumDetailsCommand(final Album album) {
+    private static String[] getAlbumArtistPair(final Album album) {
         final Artist artist = album.getArtist();
-        String artistCommand = null;
-        String artistName = null;
+        final String[] artistPair;
 
-        if (artist != null) {
-            artistName = artist.getName();
-
+        if (artist == null) {
+            artistPair = new String[]{null, null};
+        } else {
             if (album.hasAlbumArtist()) {
-                artistCommand = MPDCommand.MPD_TAG_ALBUM_ARTIST;
+                artistPair = new String[]{MPDCommand.MPD_TAG_ALBUM_ARTIST, artist.getName()};
             } else {
-                artistCommand = MPDCommand.MPD_FIND_ARTIST;
+                artistPair = new String[]{MPDCommand.MPD_TAG_ARTIST, artist.getName()};
             }
         }
+
+        return artistPair;
+    }
+
+    private static MPDCommand getAlbumDetailsCommand(final Album album) {
+        final String[] artistPair = getAlbumArtistPair(album);
 
         return new MPDCommand(MPDCommand.MPD_CMD_COUNT,
                 MPDCommand.MPD_TAG_ALBUM, album.getName(),
-                artistCommand, artistName);
+                artistPair[0], artistPair[1]);
     }
 
     private static MPDCommand getSongsCommand(final Album album) {
-        final String albumName = album.getName();
-        final Artist artist = album.getArtist();
-        String artistName = null;
-        String artistCommand = null;
+        final String[] artistPair = getAlbumArtistPair(album);
 
-        if (artist != null) {
-            artistName = artist.getName();
-
-            if (album.hasAlbumArtist()) {
-                artistCommand = MPDCommand.MPD_TAG_ALBUM_ARTIST;
-            } else {
-                artistCommand = MPDCommand.MPD_TAG_ARTIST;
-            }
-        }
-
-        return new MPDCommand(MPDCommand.MPD_CMD_FIND, MPDCommand.MPD_TAG_ALBUM, albumName,
-                artistCommand, artistName);
+        return new MPDCommand(MPDCommand.MPD_CMD_FIND, MPDCommand.MPD_TAG_ALBUM, album.getName(),
+                artistPair[0], artistPair[1]);
     }
 
     /*
@@ -216,10 +208,13 @@ public class MPD {
         final CommandQueue commandQueue;
 
         if (isCommandAvailable(MPDCommand.MPD_CMD_FIND_ADD)) {
+            final String[] artistPair = getAlbumArtistPair(album);
+
             commandQueue = new CommandQueue();
 
             commandQueue
-                    .add(MPDCommand.MPD_CMD_FIND_ADD, MPDCommand.MPD_FIND_ALBUM, album.getName());
+                    .add(MPDCommand.MPD_CMD_FIND_ADD, MPDCommand.MPD_TAG_ALBUM, album.getName(),
+                            artistPair[0], artistPair[1]);
         } else {
             final List<Music> songs = getSongs(album);
             commandQueue = MPDPlaylist.addAllCommand(songs);
@@ -256,7 +251,7 @@ public class MPD {
             commandQueue = new CommandQueue();
 
             commandQueue
-                    .add(MPDCommand.MPD_CMD_FIND_ADD, MPDCommand.MPD_FIND_ARTIST, artist.getName());
+                    .add(MPDCommand.MPD_CMD_FIND_ADD, MPDCommand.MPD_TAG_ARTIST, artist.getName());
         } else {
             final List<Music> songs = getSongs(artist);
             commandQueue = MPDPlaylist.addAllCommand(songs);
@@ -413,8 +408,10 @@ public class MPD {
     public void addToPlaylist(final String playlistName, final Album album)
             throws IOException, MPDException {
         if (mIdleConnection.isCommandAvailable(MPDCommand.MPD_CMD_SEARCH_ADD_PLAYLIST)) {
+            final String[] artistPair = getAlbumArtistPair(album);
+
             mConnection.sendCommand(MPDCommand.MPD_CMD_SEARCH_ADD_PLAYLIST, playlistName,
-                    MPDCommand.MPD_SEARCH_ALBUM, album.getName());
+                    MPDCommand.MPD_SEARCH_ALBUM, album.getName(), artistPair[0], artistPair[1]);
         } else {
             addToPlaylist(playlistName, new ArrayList<>(getSongs(album)));
         }

JMPDComm/src/main/java/org/a0z/mpd/MPDCommand.java
@@ -138,10 +138,6 @@ public class MPDCommand {
     // deprecated commands
     public static final String MPD_CMD_VOLUME = "volume";
 
-    public static final String MPD_FIND_ALBUM = "album";
-
-    public static final String MPD_FIND_ARTIST = "artist";
-
     public static final String MPD_LIST_RESPONSE_ARTIST = "Artist";
 
     public static final String MPD_SEARCH_ALBUM = "album";