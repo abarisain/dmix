abarisain/dmix

commit 62b4bd3cefd21ec4fd653edb63b0955932e8ea4c

Extra: Use a string literal to each item for intent extra use.
This also abstracts out remaining Items to keep the new string literals as
Android only.

MPDroid/src/main/java/com/namelessdev/mpdroid/library/PlaylistEditActivity.java
@@ -26,7 +26,9 @@ import org.a0z.mpd.MPDPlaylist;
 import org.a0z.mpd.MPDStatus;
 import org.a0z.mpd.event.StatusChangeListener;
 import org.a0z.mpd.exception.MPDException;
+import org.a0z.mpd.item.Artist;
 import org.a0z.mpd.item.Music;
+import org.a0z.mpd.item.PlaylistFile;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -142,7 +144,7 @@ public class PlaylistEditActivity extends MPDroidActivities.MPDroidActivity
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mPlaylistName = getIntent().getStringExtra("playlist");
+        mPlaylistName = getIntent().getStringExtra(PlaylistFile.EXTRA);
         if (null != mPlaylistName && !mPlaylistName.isEmpty()) {
             mIsPlayQueue = false;
         }
@@ -289,13 +291,15 @@ public class PlaylistEditActivity extends MPDroidActivities.MPDroidActivity
             // Copy list to avoid concurrent exception
             for (final Music music : new ArrayList<>(musics)) {
                 final HashMap<String, Object> item = new HashMap<>();
+
                 if (mIsPlayQueue) {
                     item.put("songid", music.getSongId());
                 } else {
                     item.put("songid", playlistPosition);
                 }
+
                 playlistPosition++;
-                item.put("artist", music.getArtist());
+                item.put(Artist.EXTRA, music.getArtist());
                 item.put("title", music.getTitle());
                 item.put("marked", false);
                 if (mIsPlayQueue && music.getSongId() == playingID) {

JMPDComm/backends/android/src/main/java/org/a0z/mpd/item/Album.java
@@ -49,6 +49,8 @@ public class Album extends AbstractAlbum implements Parcelable {
         }
     };
 
+    public static final String EXTRA = AbstractAlbum.TAG;
+
     protected Album(final String name, final Artist artist, final boolean hasAlbumArtist,
             final long songCount, final long duration, final long year, final String path) {
         super(name, artist, hasAlbumArtist, songCount, duration, year, path);

JMPDComm/backends/android/src/main/java/org/a0z/mpd/item/Directory.java
@@ -27,57 +27,42 @@
 
 package org.a0z.mpd.item;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import java.util.Map;
 
 /**
- * This is the Android backend {@code Artist} item.
+ * This is the Android backend {@code Directory} item.
  *
- * @see org.a0z.mpd.item.AbstractArtist For generic {@code Artist} code.
+ * @see org.a0z.mpd.item.AbstractDirectory For generic {@code Directory} code.
  */
-public class Artist extends AbstractArtist<Artist> implements Parcelable {
+public class Directory extends AbstractDirectory {
 
-    public static final Creator<Artist> CREATOR =
-            new Creator<Artist>() {
+    public static final String EXTRA = AbstractDirectory.TAG;
 
-                @Override
-                public Artist createFromParcel(final Parcel source) {
-                    return new Artist(source);
-                }
-
-                @Override
-                public Artist[] newArray(final int size) {
-                    return new Artist[size];
-                }
-            };
-
-    public static final String EXTRA = AbstractArtist.TAG;
-
-    public Artist(final Artist artist) {
-        super(artist);
+    /**
+     * Creates a new directory.
+     *
+     * @param parent   The parent directory to this directory.
+     * @param filename The filename of this directory.
+     */
+    protected Directory(final Directory parent, final String filename) {
+        super(parent, filename);
     }
 
-    public Artist(final String name) {
-        super(name);
-    }
-
-    protected Artist(final String name, final String sort) {
-        super(name, sort);
-    }
-
-    protected Artist(final Parcel in) {
-        super(in.readString(), /** name */
-                in.readString()); /** sort */
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(final Parcel dest, final int flags) {
-        dest.writeString(getName());
-        dest.writeString(sortText());
+    /**
+     * The base constructor.
+     *
+     * @param parent           The parent directory to this directory.
+     * @param filename         The filename of this directory.
+     * @param name             The name of this directory.
+     * @param directoryEntries Children directories to this directory.
+     * @param fileEntries      Children files to this directory.
+     * @param playlistEntries  Children playlists to this directory.
+     */
+    public Directory(final Directory parent, final String filename,
+            final String name,
+            final Map<String, Directory> directoryEntries,
+            final Map<String, Music> fileEntries,
+            final Map<String, PlaylistFile> playlistEntries) {
+        super(parent, filename, name, directoryEntries, fileEntries, playlistEntries);
     }
 }

JMPDComm/backends/android/src/main/java/org/a0z/mpd/item/Genre.java
@@ -35,7 +35,7 @@ import android.os.Parcelable;
  *
  * @see org.a0z.mpd.item.AbstractGenre For generic {@code Genre} code.
  */
-public class Genre extends AbstractGenre implements Parcelable {
+public class Genre extends AbstractGenre<Genre> implements Parcelable {
 
     public static final Creator<Genre> CREATOR = new Creator<Genre>() {
         @Override
@@ -48,6 +48,8 @@ public class Genre extends AbstractGenre implements Parcelable {
             return new Genre[size];
         }
     };
+
+    public static final String EXTRA = AbstractGenre.TAG;
 
     public Genre(final Genre genre) {
         super(genre);

JMPDComm/backends/android/src/main/java/org/a0z/mpd/item/PlaylistFile.java
@@ -27,57 +27,11 @@
 
 package org.a0z.mpd.item;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+public class PlaylistFile extends AbstractPlaylistFile<PlaylistFile> {
 
-/**
- * This is the Android backend {@code Artist} item.
- *
- * @see org.a0z.mpd.item.AbstractArtist For generic {@code Artist} code.
- */
-public class Artist extends AbstractArtist<Artist> implements Parcelable {
+    public static final String EXTRA = AbstractPlaylistFile.TAG;
 
-    public static final Creator<Artist> CREATOR =
-            new Creator<Artist>() {
-
-                @Override
-                public Artist createFromParcel(final Parcel source) {
-                    return new Artist(source);
-                }
-
-                @Override
-                public Artist[] newArray(final int size) {
-                    return new Artist[size];
-                }
-            };
-
-    public static final String EXTRA = AbstractArtist.TAG;
-
-    public Artist(final Artist artist) {
-        super(artist);
-    }
-
-    public Artist(final String name) {
-        super(name);
-    }
-
-    protected Artist(final String name, final String sort) {
-        super(name, sort);
-    }
-
-    protected Artist(final Parcel in) {
-        super(in.readString(), /** name */
-                in.readString()); /** sort */
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(final Parcel dest, final int flags) {
-        dest.writeString(getName());
-        dest.writeString(sortText());
+    public PlaylistFile(final String path) {
+        super(path);
     }
 }

JMPDComm/backends/android/src/main/java/org/a0z/mpd/item/Stream.java
@@ -27,40 +27,16 @@
 
 package org.a0z.mpd.item;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
- * Represents a playlist in the database
+ * This is the Android backend {@code Stream} item.
+ *
+ * @see org.a0z.mpd.item.AbstractStream For generic {@code Stream} code.
  */
-public class PlaylistFile extends Item implements FilesystemTreeEntry {
+public class Stream extends AbstractStream<Stream> {
 
-    private static final Pattern PLAYLIST_FILE_REGEXP = Pattern.compile("^.*/(.+)\\.(\\w+)$");
+    public static final String EXTRA = AbstractStream.TAG;
 
-    private final String mFullPath;
-
-    public PlaylistFile(final String path) {
-        super();
-        mFullPath = path;
-    }
-
-    @Override
-    public String getFullPath() {
-        return mFullPath;
-    }
-
-    @Override
-    public String getName() {
-        String result = "";
-
-        if (mFullPath != null) {
-            final Matcher matcher = PLAYLIST_FILE_REGEXP.matcher(mFullPath);
-            if (matcher.matches()) {
-                result = matcher.replaceAll("[$2] $1.$2");
-            } else {
-                result = mFullPath;
-            }
-        }
-        return result;
+    public Stream(final String name, final String url, final int pos) {
+        super(name, url, pos);
     }
 }

JMPDComm/backends/java/src/main/java/org/a0z/mpd/item/Artist.java
@@ -32,7 +32,7 @@ package org.a0z.mpd.item;
  *
  * @see org.a0z.mpd.item.AbstractArtist For generic {@code Artist} code.
  */
-public class Artist extends AbstractArtist {
+public class Artist extends AbstractArtist<Artist> {
 
     public Artist(final Artist artist) {
         super(artist);

JMPDComm/backends/java/src/main/java/org/a0z/mpd/item/Directory.java
@@ -27,57 +27,40 @@
 
 package org.a0z.mpd.item;
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import java.util.Map;
 
 /**
- * This is the Android backend {@code Artist} item.
+ * This is the Java backend {@code Directory} item.
  *
- * @see org.a0z.mpd.item.AbstractArtist For generic {@code Artist} code.
+ * @see org.a0z.mpd.item.AbstractDirectory For generic {@code Directory} code.
  */
-public class Artist extends AbstractArtist<Artist> implements Parcelable {
+public class Directory extends AbstractDirectory {
 
-    public static final Creator<Artist> CREATOR =
-            new Creator<Artist>() {
-
-                @Override
-                public Artist createFromParcel(final Parcel source) {
-                    return new Artist(source);
-                }
-
-                @Override
-                public Artist[] newArray(final int size) {
-                    return new Artist[size];
-                }
-            };
-
-    public static final String EXTRA = AbstractArtist.TAG;
-
-    public Artist(final Artist artist) {
-        super(artist);
+    /**
+     * Creates a new directory.
+     *
+     * @param parent   The parent directory to this directory.
+     * @param filename The filename of this directory.
+     */
+    protected Directory(final Directory parent, final String filename) {
+        super(parent, filename);
     }
 
-    public Artist(final String name) {
-        super(name);
-    }
-
-    protected Artist(final String name, final String sort) {
-        super(name, sort);
-    }
-
-    protected Artist(final Parcel in) {
-        super(in.readString(), /** name */
-                in.readString()); /** sort */
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(final Parcel dest, final int flags) {
-        dest.writeString(getName());
-        dest.writeString(sortText());
+    /**
+     * The base constructor.
+     *
+     * @param parent           The parent directory to this directory.
+     * @param filename         The filename of this directory.
+     * @param name             The name of this directory.
+     * @param directoryEntries Children directories to this directory.
+     * @param fileEntries      Children files to this directory.
+     * @param playlistEntries  Children playlists to this directory.
+     */
+    public Directory(final Directory parent, final String filename,
+            final String name,
+            final Map<String, Directory> directoryEntries,
+            final Map<String, Music> fileEntries,
+            final Map<String, PlaylistFile> playlistEntries) {
+        super(parent, filename, name, directoryEntries, fileEntries, playlistEntries);
     }
 }

commit 3569258157879de77ac7c46b93d64b012256df85

PlaylistEditActivity: Fix artist listing.
This fixes a regression from 62b4bd3, apparently, Artist.EXTRA went one too far.

MPDroid/src/main/java/com/namelessdev/mpdroid/library/PlaylistEditActivity.java
@@ -26,7 +26,6 @@ import org.a0z.mpd.MPDPlaylist;
 import org.a0z.mpd.MPDStatus;
 import org.a0z.mpd.event.StatusChangeListener;
 import org.a0z.mpd.exception.MPDException;
-import org.a0z.mpd.item.Artist;
 import org.a0z.mpd.item.Music;
 import org.a0z.mpd.item.PlaylistFile;
 
@@ -305,7 +304,7 @@ public class PlaylistEditActivity extends MPDroidActivities.MPDroidActivity
             }
 
             playlistPosition++;
-            item.put(Artist.EXTRA, music.getArtist());
+            item.put(Music.TAG_ARTIST, music.getArtist());
             item.put(Music.TAG_TITLE, music.getTitle());
             item.put("marked", false);
             if (mIsPlayQueue && music.getSongId() == playingID) {