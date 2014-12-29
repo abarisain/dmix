ad5c51ce1f912959acb8ac8e5ee03bf8da25769e
Arnaud Barisain-Monrose <abarisain@gmail.com>
2014-12-30 00:15:42 +0100

    Disable fast scroll on song list

--- MPDroid/src/main/java/com/namelessdev/mpdroid/fragments/SongsFragment.java
+++ MPDroid/src/main/java/com/namelessdev/mpdroid/fragments/SongsFragment.java
@@ -244,6 +244,7 @@ public class SongsFragment extends BrowseFragment {
         mList = (AbsListView) view.findViewById(R.id.list);
         registerForContextMenu(mList);
         mList.setOnItemClickListener(this);
+        mList.setFastScrollEnabled(false);
         if (mList instanceof ListView) {
             ((ListView) mList).setDivider(null);
         }
