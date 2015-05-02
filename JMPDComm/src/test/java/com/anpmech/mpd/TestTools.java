5967e361436cebd598a8db4d76546328f6c58e8f
Avuton Olrich <avuton@gmail.com>
2015-05-02 11:17:20 -0700

    JMPDComm: Add test framework.

---
+++ JMPDComm/src/test/java/com/anpmech/mpd/TestTools.java
@@ -0,0 +1,102 @@
+package com.anpmech.mpd;/*
+ * Copyright (C) 2004 Felipe Gustavo de Almeida
+ * Copyright (C) 2010-2015 The MPDroid Project
+ *
+ * All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice,this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * This class contains general tools used for testing.
+ */
+public enum TestTools {
+    ;
+
+    /**
+     * This file contains a single track lsinfo file response with multiple same-tag entries.
+     */
+    public static final String FILE_SINGULAR_MULTI_TAG_FILE = "lsinfo/singular_multi_tag_file.txt";
+
+    /**
+     * This file contains a single playlistinfo entry.
+     */
+    public static final String FILE_SINGULAR_PLAYLISTINFO
+            = "playlistinfo/singular_playlistinfo.txt";
+
+    /**
+     * This file contains a single track lsinfo file response.
+     */
+    public static final String FILE_SINGULAR_TRACK_FILE = "lsinfo/singular_track_file.txt";
+
+    /**
+     * This file contains a empty file, to test the a empty response.
+     */
+    public static final String FILE_SINGULAR_TRACK_MINIMAL
+            = "lsinfo/singular_track_file_minimal.txt";
+
+    /**
+     * This file contains a single stream lsinfo entry.
+     */
+    public static final String FILE_SINGULAR_TRACK_STREAM = "lsinfo/singular_track_stream.txt";
+
+    /**
+     * This array contains all resource files.
+     */
+    public static final String[] TEST_FILE_PATHS = {
+            FILE_SINGULAR_TRACK_FILE,
+            FILE_SINGULAR_TRACK_MINIMAL,
+            FILE_SINGULAR_TRACK_STREAM,
+            FILE_SINGULAR_MULTI_TAG_FILE,
+            FILE_SINGULAR_PLAYLISTINFO
+    };
+
+    public static String getMatchMsg(final String message, final String filePath) {
+        return message + " failed to match for filepath: " + filePath + '.';
+    }
+
+    /**
+     * Takes a file path and reads the file into a {@link String}.
+     *
+     * @param pathname The path name of the file to read into the {@code String}.
+     * @return The resulting {@code String}.
+     * @throws IOException If there was a problem reading from the file.
+     */
+    public static String readFile(final String pathname) throws IOException {
+        final ClassLoader loader = ClassLoader.getSystemClassLoader();
+        final File file = new File(loader.getResource(pathname).getFile());
+        final StringBuilder fileContents = new StringBuilder((int) file.length());
+        final String lineSeparator = System.getProperty("line.separator");
+
+        try (final Scanner scanner = new Scanner(file, "UTF-8")) {
+            while (scanner.hasNextLine()) {
+                fileContents.append(scanner.nextLine());
+                fileContents.append(lineSeparator);
+            }
+        }
+
+        return fileContents.toString();
+    }
+}
---
+++ JMPDComm/src/test/java/com/anpmech/mpd/connection/CommandResponseCreator.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2004 Felipe Gustavo de Almeida
+ * Copyright (C) 2010-2015 The MPDroid Project
+ *
+ * All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice,this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.anpmech.mpd.connection;
+
+import com.anpmech.mpd.TestTools;
+import com.anpmech.mpd.commandresponse.CommandResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * This class creates {@link CommandResponse}s from the various test resources.
+ */
+public enum CommandResponseCreator {
+    ;
+
+    public static CommandResponse getCommandResponse(final String filePath) throws IOException {
+        final String rawResult = TestTools.readFile(filePath);
+        final CommandResult result = new CommandResult(null, rawResult, null);
+
+        return new CommandResponse(result);
+    }
+
+    public static Collection<CommandResponse> getCommandResponses() throws IOException {
+        final Collection<CommandResponse> results =
+                new ArrayList<>(TestTools.TEST_FILE_PATHS.length);
+
+        for (final String file : TestTools.TEST_FILE_PATHS) {
+            results.add(getCommandResponse(file));
+        }
+
+        return results;
+    }
+
+}
