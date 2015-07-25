abarisain/dmix

commit 1c05055b55b1cf3c11169978181757af63ca7c5f

SearchActivity: Prevent a race between the PagerView / MenuItem callback.
This commit should prevent a potential race between the PagerView field
(mPager) and the MenuItem callback. The race can present itself while the menu
is loading (menu item transition animation is active) and the user uses a
second touch to change the view, the PagerView field can be correct, while the
menu item callback can pertain to the previous view. This validates the view
for equality prior to processing the menu item click.

MPDroid/src/main/java/com/namelessdev/mpdroid/SearchActivity.java
@@ -438,55 +438,67 @@ public class SearchActivity extends MPDroidActivity implements OnMenuItemClickLi
     @Override
     public boolean onMenuItemClick(final MenuItem item) {
         final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
-        final List<? extends Item<?>> targetArray;
-        switch (mPager.getCurrentItem()) {
-            case RESULT_ALBUM:
-                targetArray = mAlbumResults;
-                break;
-            case RESULT_ARTIST:
-                targetArray = mArtistResults;
-                break;
-            case RESULT_MUSIC:
-                targetArray = mSongResults;
-                break;
-            default:
-                throw new UnsupportedOperationException(UNKNOWN_ITEM_ERROR);
-        }
-        final Object selectedItem = targetArray.get((int) info.id);
-        if (item.getItemId() == GOTO_ALBUM) {
-            if (selectedItem instanceof Music) {
-                final Music music = (Music) selectedItem;
-                final Intent intent = new Intent(this, SimpleLibraryActivity.class);
-                final Parcelable artist = new Artist(music.getAlbumArtistOrArtist());
-                intent.putExtra(Artist.EXTRA, artist);
-                intent.putExtra(Album.EXTRA, music.getAlbum());
-                startActivityForResult(intent, -1);
-            }
+        final View itemFocus = (View) info.targetView.getParent();
+        final View currentFocus = getCurrentFocus();
+        final boolean isConsumed;
+
+        if (itemFocus == null || currentFocus == null || !itemFocus.equals(currentFocus)) {
+            isConsumed = false;
+            Log.w(TAG, "Ignoring menu item press due to view change.");
         } else {
-            mApp.getAsyncHelper().execAsync(new Runnable() {
-                @Override
-                public void run() {
-                    boolean replace = false;
-                    boolean play = false;
-                    switch (item.getItemId()) {
-                        case ADD_REPLACE_PLAY:
-                            replace = true;
-                            play = true;
-                            break;
-                        case ADD_REPLACE:
-                            replace = true;
-                            break;
-                        case ADD_PLAY:
-                            play = true;
-                            break;
-                        default:
-                            break;
-                    }
-                    add(selectedItem, replace, play);
+            isConsumed = true;
+            final List<? extends Item<?>> targetArray;
+
+            switch (mPager.getCurrentItem()) {
+                case RESULT_ALBUM:
+                    targetArray = mAlbumResults;
+                    break;
+                case RESULT_ARTIST:
+                    targetArray = mArtistResults;
+                    break;
+                case RESULT_MUSIC:
+                    targetArray = mSongResults;
+                    break;
+                default:
+                    throw new UnsupportedOperationException(UNKNOWN_ITEM_ERROR);
+            }
+            final Object selectedItem = targetArray.get((int) info.id);
+            if (item.getItemId() == GOTO_ALBUM) {
+                if (selectedItem instanceof Music) {
+                    final Music music = (Music) selectedItem;
+                    final Intent intent = new Intent(this, SimpleLibraryActivity.class);
+                    final Parcelable artist = new Artist(music.getAlbumArtistOrArtist());
+                    intent.putExtra(Artist.EXTRA, artist);
+                    intent.putExtra(Album.EXTRA, music.getAlbum());
+                    startActivityForResult(intent, -1);
                 }
-            });
+            } else {
+                mApp.getAsyncHelper().execAsync(new Runnable() {
+                    @Override
+                    public void run() {
+                        boolean replace = false;
+                        boolean play = false;
+                        switch (item.getItemId()) {
+                            case ADD_REPLACE_PLAY:
+                                replace = true;
+                                play = true;
+                                break;
+                            case ADD_REPLACE:
+                                replace = true;
+                                break;
+                            case ADD_PLAY:
+                                play = true;
+                                break;
+                            default:
+                                break;
+                        }
+                        add(selectedItem, replace, play);
+                    }
+                });
+            }
         }
-        return false;
+
+        return isConsumed;
     }
 
     @Override

commit 11c23d7514599551f2ccd5f69d1c3a2967db2389

SearchActivity: Fix song add, broken by 1c05055b5.
The view focus can be null, if so, ignore it for this case. This might not be a
completely correct fix, but, hopefully, we'll avoid most races fixed by that
commit.

MPDroid/src/main/java/com/namelessdev/mpdroid/SearchActivity.java
@@ -442,7 +442,7 @@ public class SearchActivity extends MPDroidActivity implements OnMenuItemClickLi
         final View currentFocus = getCurrentFocus();
         final boolean isConsumed;
 
-        if (itemFocus == null || currentFocus == null || !itemFocus.equals(currentFocus)) {
+        if (itemFocus == null || currentFocus != null && !itemFocus.equals(currentFocus)) {
             isConsumed = false;
             Log.w(TAG, "Ignoring menu item press due to view change.");
         } else {